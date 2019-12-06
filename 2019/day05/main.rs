7020659ca8abeba7693d0aea26c6e9e2b4c74297
sugyan <sugi1982@gmail.com>
2019-12-06 09:58:10 +0900

    WIP: day05/2 (still has bugs...)

--- 2019/day05/main.rs
+++ 2019/day05/main.rs
@@ -1,39 +1,27 @@
+use std::env;
 use std::io::stdin;
 
-fn solve1(codes: Vec<i32>) -> i32 {
-    let input = 1;
+fn solve(codes: Vec<i32>, input: i32) -> i32 {
     let mut v = codes;
     let mut i = 0;
     loop {
+        println!("[{}] {}", i, v[i]);
+        let get_param = |pos: usize| -> i32 {
+            return if (v[i] / 10i32.pow(pos as u32 + 1)) % 10 == 0 {
+                v[v[i + pos] as usize]
+            } else {
+                v[i + pos]
+            };
+        };
         match v[i] % 100 {
             1 => {
-                let lhs = if (v[i] / 100) % 10 == 0 {
-                    v[v[i + 1] as usize]
-                } else {
-                    v[i + 1]
-                };
-                let rhs = if (v[i] / 1000) % 10 == 0 {
-                    v[v[i + 2] as usize]
-                } else {
-                    v[i + 2]
-                };
-                let pos = v[i + 3] as usize;
-                v[pos] = lhs + rhs;
+                let pos = get_param(3) as usize;
+                v[pos] = get_param(1) + get_param(2);
                 i += 4;
             }
             2 => {
-                let lhs = if (v[i] / 100) % 10 == 0 {
-                    v[v[i + 1] as usize]
-                } else {
-                    v[i + 1]
-                };
-                let rhs = if (v[i] / 1000) % 10 == 0 {
-                    v[v[i + 2] as usize]
-                } else {
-                    v[i + 2]
-                };
-                let pos = v[i + 3] as usize;
-                v[pos] = lhs * rhs;
+                let pos = get_param(3) as usize;
+                v[pos] = get_param(1) * get_param(2);
                 i += 4;
             }
             3 => {
@@ -42,12 +30,32 @@ fn solve1(codes: Vec<i32>) -> i32 {
                 i += 2;
             }
             4 => {
-                v[0] = if v[i] > 100 {
-                    v[i + 1]
-                } else {
-                    v[v[i + 1] as usize]
-                };
+                v[0] = get_param(1);
                 i += 2;
+            }
+            5 => {
+                if get_param(1) != 0 {
+                    i = get_param(2) as usize;
+                } else {
+                    i += 3;
+                }
+            }
+            6 => {
+                if get_param(1) == 0 {
+                    i = get_param(2) as usize;
+                } else {
+                    i += 3;
+                }
+            }
+            7 => {
+                let pos = get_param(3) as usize;
+                v[pos] = if get_param(1) < get_param(2) { 1 } else { 0 };
+                i += 4;
+            }
+            8 => {
+                let pos = get_param(3) as usize;
+                v[pos] = if get_param(1) == get_param(2) { 1 } else { 0 };
+                i += 4;
             }
             99 => break,
             _ => {}
@@ -57,9 +65,15 @@ fn solve1(codes: Vec<i32>) -> i32 {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     let v: Vec<i32> = buf.split(",").map(|s| s.trim().parse().unwrap()).collect();
-    let answer = solve1(v);
+    let answer = if args.len() < 2 || &args[1] != "2" {
+        solve(v, 1)
+    } else {
+        solve(v, 8)
+    };
     println!("{}", answer);
 }
