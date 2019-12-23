1766bd3a36e9c81b4c08e509a7650b00754f1877
sugyan <sugi1982@gmail.com>
2019-12-23 22:39:51 +0900

    Add 2019/day23 part2

--- 2019/day23/main.rs
+++ 2019/day23/main.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 use std::io::stdin;
 use utils::IntCode;
 
-fn solve1(codes: Vec<i64>) -> i64 {
+fn solve(codes: Vec<i64>) -> (Option<i64>, Option<i64>) {
     let mut computers: Vec<IntCode> = Vec::with_capacity(50);
     let mut inq: Vec<VecDeque<i64>> = vec![VecDeque::new(); 50];
     let mut outq: Vec<VecDeque<i64>> = vec![VecDeque::new(); 50];
@@ -11,7 +11,21 @@ fn solve1(codes: Vec<i64>) -> i64 {
         computer.run(vec![i as i64]);
         computers.push(computer);
     }
+    let mut answers = (None, None);
+    let mut nat = (0, 0);
+    let mut prev: Option<i64> = None;
     loop {
+        if inq.iter().all(|q| q.is_empty()) {
+            inq[0].push_back(nat.0);
+            inq[0].push_back(nat.1);
+            if let Some(p) = prev {
+                if p == nat.1 {
+                    answers.1 = Some(nat.1);
+                    break;
+                }
+            }
+            prev = Some(nat.1);
+        }
         for (i, c) in (0..).zip(computers.iter_mut()) {
             let mut inputs = vec![];
             if inq[i].is_empty() {
@@ -33,18 +47,29 @@ fn solve1(codes: Vec<i64>) -> i64 {
                 let x = outq[i].pop_front().unwrap();
                 let y = outq[i].pop_front().unwrap();
                 if address == 255 {
-                    return y;
+                    nat = (x, y);
+                    if answers.0.is_none() {
+                        answers.0 = Some(y);
+                    }
+                } else {
+                    inq[address as usize].push_back(x);
+                    inq[address as usize].push_back(y);
                 }
-                inq[address as usize].push_back(x);
-                inq[address as usize].push_back(y);
             }
         }
     }
+    return answers;
 }
 
 fn main() {
     let mut buf = String::new();
     stdin().read_line(&mut buf).ok();
     let codes: Vec<i64> = buf.split(",").map(|s| s.trim().parse().unwrap()).collect();
-    println!("{}", solve1(codes));
+    let answers = solve(codes);
+    if let Some(answer1) = answers.0 {
+        println!("{}", answer1);
+    }
+    if let Some(answer2) = answers.1 {
+        println!("{}", answer2);
+    }
 }
