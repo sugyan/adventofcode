7f4f4fedbe5d046d8381d6b2a5b394ef1c3e4970
sugyan <sugi1982@gmail.com>
2020-12-01 17:50:58 +0900

    Add 2020/day01 part2

--- 2020/day01/src/main.rs
+++ 2020/day01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::io::{BufRead, BufReader};
 
 struct Solution {
@@ -8,11 +9,23 @@ impl Solution {
     fn new(inputs: Vec<i32>) -> Self {
         Self { inputs }
     }
-    fn solve(&self) -> i32 {
+    fn solve_1(&self) -> i32 {
+        let hs: HashSet<&i32> = self.inputs.iter().collect();
+        for &i in self.inputs.iter() {
+            if hs.contains(&(2020 - i)) {
+                return (2020 - i) * i;
+            }
+        }
+        0
+    }
+    fn solve_2(&self) -> i32 {
+        let hs: HashSet<&i32> = self.inputs.iter().collect();
         for i in 0..self.inputs.len() - 1 {
-            for j in i..self.inputs.len() {
-                if self.inputs[i] + self.inputs[j] == 2020 {
-                    return self.inputs[i] * self.inputs[j];
+            for j in i + 1..self.inputs.len() {
+                if hs.contains(&(2020 - self.inputs[i] - self.inputs[j])) {
+                    return (2020 - self.inputs[i] - self.inputs[j])
+                        * self.inputs[i]
+                        * self.inputs[j];
                 }
             }
         }
@@ -26,7 +39,9 @@ fn main() {
         .filter_map(|line| line.ok())
         .filter_map(|s| s.parse().ok())
         .collect();
-    println!("{}", Solution::new(inputs).solve());
+    let solution = Solution::new(inputs);
+    println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -36,10 +51,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn example_1() {
         assert_eq!(
             514579,
-            Solution::new(vec![1721, 979, 366, 299, 675, 1456]).solve()
+            Solution::new(vec![1721, 979, 366, 299, 675, 1456]).solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            241861950,
+            Solution::new(vec![1721, 979, 366, 299, 675, 1456]).solve_2()
         );
     }
 }
