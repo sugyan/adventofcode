db0ced24c3aa7023a901be7a0ef3eaa9c025bc35
sugyan <sugi1982@gmail.com>
2020-12-03 15:26:26 +0900

    Add 2020/day03 part2

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -14,9 +14,18 @@ impl Solution {
         }
     }
     fn solve_1(&self) -> usize {
+        self.count(3, 1)
+    }
+    fn solve_2(&self) -> usize {
+        [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+            .iter()
+            .map(|&(right, down)| self.count(right, down))
+            .product()
+    }
+    fn count(&self, right: usize, down: usize) -> usize {
         let width = self.grid[0].len();
-        (0..self.grid.len())
-            .filter(|&i| self.grid[i][(i * 3) % width])
+        (0..self.grid.len() / down)
+            .filter(|&i| self.grid[i * down][(i * right) % width])
             .count()
     }
 }
@@ -26,7 +35,9 @@ fn main() {
         .lines()
         .filter_map(|line| line.ok())
         .collect();
-    println!("{}", Solution::new(inputs).solve_1());
+    let solution = Solution::new(inputs);
+    println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -55,6 +66,31 @@ mod tests {
                     .collect()
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            336,
+            Solution::new(
+                "
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"[1..]
+                    .split('\n')
+                    .map(|s| s.to_string())
+                    .collect()
+            )
+            .solve_2()
         );
     }
 }
