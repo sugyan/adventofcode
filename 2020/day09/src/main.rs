47d0ce24f6e0fee8eae125bfad87caf8635aeb31
sugyan <sugi1982@gmail.com>
2020-12-09 15:04:57 +0900

    Add 2020/day09 part2

--- 2020/day09/src/main.rs
+++ 2020/day09/src/main.rs
@@ -22,6 +22,26 @@ impl Solution {
         }
         0
     }
+    fn solve_2(&self) -> u64 {
+        let target = self.solve_1();
+        let (mut l, mut r) = (0, 0);
+        let mut sum = self.inputs[0];
+        while sum != target {
+            if sum < target {
+                r += 1;
+                sum += self.inputs[r];
+            } else {
+                sum -= self.inputs[l];
+                l += 1;
+            }
+        }
+        let (mut min, mut max) = (std::u64::MAX, std::u64::MIN);
+        for &n in self.inputs.iter().skip(l).take(r - l + 1) {
+            min = std::cmp::min(min, n);
+            max = std::cmp::max(max, n);
+        }
+        min + max
+    }
 }
 
 fn main() {
@@ -34,6 +54,7 @@ fn main() {
         25,
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -52,6 +73,21 @@ mod tests {
                 5
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            62,
+            Solution::new(
+                vec![
+                    35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277,
+                    309, 576,
+                ],
+                5
+            )
+            .solve_2()
         );
     }
 }
