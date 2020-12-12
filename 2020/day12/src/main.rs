2c435b1dc47cfd501c281407e50c79a0a829bbc6
sugyan <sugi1982@gmail.com>
2020-12-12 15:31:01 +0900

    Add 2020/day12 part2

--- 2020/day12/src/main.rs
+++ 2020/day12/src/main.rs
@@ -1,42 +1,84 @@
 use std::io::{BufRead, BufReader};
 
 struct Solution {
-    inputs: Vec<String>,
+    instructions: Vec<(char, i32)>,
 }
 
 impl Solution {
     fn new(inputs: Vec<String>) -> Self {
-        Self { inputs }
+        Self {
+            instructions: inputs
+                .iter()
+                .filter_map(|input| {
+                    if let (Some(action), Ok(value)) =
+                        (input.chars().next(), input[1..].parse::<i32>())
+                    {
+                        Some((action, value))
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+        }
     }
     fn solve_1(&self) -> i32 {
         let mut p = (0, 0);
         let mut d = (1, 0);
-        for input in self.inputs.iter() {
-            if let Ok(value) = input[1..].parse::<i32>() {
-                match input.chars().next() {
-                    Some('N') => p.1 += value,
-                    Some('S') => p.1 -= value,
-                    Some('E') => p.0 += value,
-                    Some('W') => p.0 -= value,
-                    Some('L') => {
-                        d = match value {
-                            90 => (-d.1, d.0),
-                            180 => (-d.0, -d.1),
-                            270 => (d.1, -d.0),
-                            _ => d,
-                        }
+        for &instruction in self.instructions.iter() {
+            match instruction.0 {
+                'N' => p.1 += instruction.1,
+                'S' => p.1 -= instruction.1,
+                'E' => p.0 += instruction.1,
+                'W' => p.0 -= instruction.1,
+                'L' => {
+                    d = match instruction.1 {
+                        90 => (-d.1, d.0),
+                        180 => (-d.0, -d.1),
+                        270 => (d.1, -d.0),
+                        _ => d,
                     }
-                    Some('R') => {
-                        d = match value {
-                            90 => (d.1, -d.0),
-                            180 => (-d.0, -d.1),
-                            270 => (-d.1, d.0),
-                            _ => d,
-                        }
-                    }
-                    Some('F') => p = (p.0 + d.0 * value, p.1 + d.1 * value),
-                    _ => {}
                 }
+                'R' => {
+                    d = match instruction.1 {
+                        90 => (d.1, -d.0),
+                        180 => (-d.0, -d.1),
+                        270 => (-d.1, d.0),
+                        _ => d,
+                    }
+                }
+                'F' => p = (p.0 + d.0 * instruction.1, p.1 + d.1 * instruction.1),
+                _ => {}
+            }
+        }
+        p.0.abs() + p.1.abs()
+    }
+    fn solve_2(&self) -> i32 {
+        let mut p = (0, 0);
+        let mut w = (10, 1);
+        for &instruction in self.instructions.iter() {
+            match instruction.0 {
+                'N' => w.1 += instruction.1,
+                'S' => w.1 -= instruction.1,
+                'E' => w.0 += instruction.1,
+                'W' => w.0 -= instruction.1,
+                'L' => {
+                    w = match instruction.1 {
+                        90 => (-w.1, w.0),
+                        180 => (-w.0, -w.1),
+                        270 => (w.1, -w.0),
+                        _ => w,
+                    }
+                }
+                'R' => {
+                    w = match instruction.1 {
+                        90 => (w.1, -w.0),
+                        180 => (-w.0, -w.1),
+                        270 => (-w.1, w.0),
+                        _ => w,
+                    }
+                }
+                'F' => p = (p.0 + w.0 * instruction.1, p.1 + w.1 * instruction.1),
+                _ => {}
             }
         }
         p.0.abs() + p.1.abs()
@@ -51,6 +93,7 @@ fn main() {
             .collect(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -73,6 +116,25 @@ F11"[1..]
                     .collect()
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            286,
+            Solution::new(
+                "
+F10
+N3
+F7
+R90
+F11"[1..]
+                    .split('\n')
+                    .map(|s| s.to_string())
+                    .collect()
+            )
+            .solve_2()
         );
     }
 }
