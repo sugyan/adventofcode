f5b166c3f527c3872bdc4a164c19af8518d7f0a6
sugyan <sugi1982@gmail.com>
2020-12-12 15:38:50 +0900

    Update 2020/day12

--- 2020/day12/src/main.rs
+++ 2020/day12/src/main.rs
@@ -22,62 +22,35 @@ impl Solution {
         }
     }
     fn solve_1(&self) -> i32 {
-        let mut p = (0, 0);
-        let mut d = (1, 0);
-        for &instruction in self.instructions.iter() {
-            match instruction.0 {
-                'N' => p.1 += instruction.1,
-                'S' => p.1 -= instruction.1,
-                'E' => p.0 += instruction.1,
-                'W' => p.0 -= instruction.1,
-                'L' => {
-                    d = match instruction.1 {
-                        90 => (-d.1, d.0),
-                        180 => (-d.0, -d.1),
-                        270 => (d.1, -d.0),
-                        _ => d,
-                    }
-                }
-                'R' => {
-                    d = match instruction.1 {
-                        90 => (d.1, -d.0),
-                        180 => (-d.0, -d.1),
-                        270 => (-d.1, d.0),
-                        _ => d,
-                    }
-                }
-                'F' => p = (p.0 + d.0 * instruction.1, p.1 + d.1 * instruction.1),
-                _ => {}
-            }
-        }
-        p.0.abs() + p.1.abs()
+        self.run((1, 0), false)
     }
     fn solve_2(&self) -> i32 {
+        self.run((10, 1), true)
+    }
+    fn run(&self, w: (i32, i32), waypoint: bool) -> i32 {
         let mut p = (0, 0);
-        let mut w = (10, 1);
-        for &instruction in self.instructions.iter() {
-            match instruction.0 {
-                'N' => w.1 += instruction.1,
-                'S' => w.1 -= instruction.1,
-                'E' => w.0 += instruction.1,
-                'W' => w.0 -= instruction.1,
+        let mut w = w;
+        for &inst in self.instructions.iter() {
+            match inst.0 {
+                'N' if waypoint => w.1 += inst.1,
+                'N' => p.1 += inst.1,
+                'S' if waypoint => w.1 -= inst.1,
+                'S' => p.1 -= inst.1,
+                'E' if waypoint => w.0 += inst.1,
+                'E' => p.0 += inst.1,
+                'W' if waypoint => w.0 -= inst.1,
+                'W' => p.0 -= inst.1,
                 'L' => {
-                    w = match instruction.1 {
-                        90 => (-w.1, w.0),
-                        180 => (-w.0, -w.1),
-                        270 => (w.1, -w.0),
-                        _ => w,
+                    for _ in 0..inst.1 / 90 {
+                        w = (-w.1, w.0)
                     }
                 }
                 'R' => {
-                    w = match instruction.1 {
-                        90 => (w.1, -w.0),
-                        180 => (-w.0, -w.1),
-                        270 => (-w.1, w.0),
-                        _ => w,
+                    for _ in 0..inst.1 / 90 {
+                        w = (w.1, -w.0)
                     }
                 }
-                'F' => p = (p.0 + w.0 * instruction.1, p.1 + w.1 * instruction.1),
+                'F' => p = (p.0 + w.0 * inst.1, p.1 + w.1 * inst.1),
                 _ => {}
             }
         }
