f3bdd856d2a855c1e50fbca146f639b0677d778f
sugyan <sugi1982@gmail.com>
2020-12-13 22:20:45 +0900

    Add 2020/day13 part2

--- 2020/day13/src/main.rs
+++ 2020/day13/src/main.rs
@@ -21,6 +21,38 @@ impl Solution {
             0
         }
     }
+    fn solve_2(&self) -> i64 {
+        let v: Vec<(usize, i64)> = self.inputs[1]
+            .split(',')
+            .enumerate()
+            .filter_map(|(i, s)| {
+                if let Ok(id) = s.parse::<i64>() {
+                    Some((i, id))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        let mut n = (0, 1);
+        for &e in v.iter().skip(1) {
+            let d = Solution::common_target((-(v[0].0 as i64), v[0].1), (-(e.0 as i64), e.1));
+            n.0 = Solution::common_target(n, (d / v[0].1, e.1));
+            n.1 *= e.1;
+        }
+        n.0 * v[0].1
+    }
+    fn common_target(p1: (i64, i64), p2: (i64, i64)) -> i64 {
+        let mut n1 = p1.0;
+        let mut n2 = p2.0;
+        loop {
+            match n1.cmp(&n2) {
+                std::cmp::Ordering::Less => n1 += ((n2 - n1 - 1) / p1.1 + 1) * p1.1,
+                std::cmp::Ordering::Equal => break,
+                std::cmp::Ordering::Greater => n2 += ((n1 - n2 - 1) / p2.1 + 1) * p2.1,
+            }
+        }
+        n1
+    }
 }
 
 fn main() {
@@ -31,6 +63,7 @@ fn main() {
             .collect(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -50,6 +83,34 @@ mod tests {
                     .collect()
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            1068781,
+            Solution::new(vec![String::new(), String::from("7,13,x,x,59,x,31,19")]).solve_2()
+        );
+        assert_eq!(
+            3417,
+            Solution::new(vec![String::new(), String::from("17,x,13,19")]).solve_2()
+        );
+        assert_eq!(
+            754018,
+            Solution::new(vec![String::new(), String::from("67,7,59,61")]).solve_2()
+        );
+        assert_eq!(
+            779210,
+            Solution::new(vec![String::new(), String::from("67,x,7,59,61")]).solve_2()
+        );
+        assert_eq!(
+            1261476,
+            Solution::new(vec![String::new(), String::from("67,7,x,59,61")]).solve_2()
+        );
+        assert_eq!(
+            1202161486,
+            Solution::new(vec![String::new(), String::from("1789,37,47,1889")]).solve_2()
         );
     }
 }
