8e258dc0d1a35f120e70efaa54148bb8afd86991
sugyan <sugi1982@gmail.com>
2020-12-14 16:29:27 +0900

    Add 2020/day14 part2

--- 2020/day14/src/main.rs
+++ 2020/day14/src/main.rs
@@ -1,32 +1,67 @@
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::io::{BufRead, BufReader};
 
 struct Solution {
     inputs: Vec<String>,
+    re: Regex,
 }
 
 impl Solution {
     fn new(inputs: Vec<String>) -> Self {
-        Self { inputs }
+        Self {
+            inputs,
+            re: Regex::new(r"^mem\[(\d+)\] = (\d+)$").unwrap(),
+        }
     }
     fn solve_1(&self) -> u64 {
-        let re = Regex::new(r"^mem\[(\d+)\] = (\d+)$").unwrap();
-        let mut masks: (u64, u64) = ((1 << 36) - 1, 0);
+        let mut mask: (u64, u64) = ((1 << 36) - 1, 0);
         let mut mem: HashMap<u64, u64> = HashMap::new();
         for input in self.inputs.iter() {
-            if let Some(mask) = input.strip_prefix("mask = ") {
-                masks = ((1 << 36) - 1, 0);
-                for (i, c) in mask.chars().rev().enumerate() {
+            if let Some(m) = input.strip_prefix("mask = ") {
+                mask = ((1 << 36) - 1, 0);
+                for (i, c) in m.chars().rev().enumerate() {
                     match c {
-                        '0' => masks.0 &= !(1 << i),
-                        '1' => masks.1 |= 1 << i,
+                        '0' => mask.0 &= !(1 << i),
+                        '1' => mask.1 |= 1 << i,
                         _ => {}
                     }
                 }
-            } else if let Some(cap) = re.captures(input) {
+            } else if let Some(cap) = self.re.captures(input) {
                 if let (Ok(address), Ok(value)) = (cap[1].parse::<u64>(), cap[2].parse::<u64>()) {
-                    mem.insert(address, value & masks.0 | masks.1);
+                    mem.insert(address, value & mask.0 | mask.1);
+                }
+            }
+        }
+        mem.values().sum()
+    }
+    fn solve_2(&self) -> u64 {
+        let mut mem: HashMap<u64, u64> = HashMap::new();
+        let mut mask = "";
+        for input in self.inputs.iter() {
+            if let Some(m) = input.strip_prefix("mask = ") {
+                mask = m;
+            } else if let Some(cap) = self.re.captures(input) {
+                if let (Ok(address), Ok(value)) = (cap[1].parse::<u64>(), cap[2].parse::<u64>()) {
+                    let mut vd: VecDeque<u64> = VecDeque::new();
+                    vd.push_back(address);
+                    for (i, c) in mask.chars().rev().enumerate() {
+                        match c {
+                            '1' => vd.iter_mut().for_each(|v| *v |= 1 << i),
+                            'X' => {
+                                for _ in 0..vd.len() {
+                                    if let Some(front) = vd.pop_front() {
+                                        vd.push_back(front | 1 << i);
+                                        vd.push_back(front & !(1 << i));
+                                    }
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                    for &address in vd.iter() {
+                        mem.insert(address, value);
+                    }
                 }
             }
         }
@@ -42,6 +77,7 @@ fn main() {
             .collect(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -63,6 +99,25 @@ mem[8] = 0"[1..]
                     .collect()
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            208,
+            Solution::new(
+                "
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+"[1..]
+                    .split('\n')
+                    .map(|s| s.to_string())
+                    .collect()
+            )
+            .solve_2()
         );
     }
 }
