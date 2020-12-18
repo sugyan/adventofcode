a143b76f06a58c44244cfe2b3818062ba9b38d37
sugyan <sugi1982@gmail.com>
2020-12-18 17:59:37 +0900

    Add 2020/day18 part2

--- 2020/day18/src/main.rs
+++ 2020/day18/src/main.rs
@@ -4,26 +4,38 @@ struct Solution {
     inputs: Vec<String>,
 }
 
+#[derive(Copy, Clone)]
+enum Op {
+    Add,
+    Mul,
+}
+
 impl Solution {
     fn new(inputs: Vec<String>) -> Self {
         Self { inputs }
     }
     fn solve_1(&self) -> u64 {
-        self.inputs.iter().map(|s| Solution::evaluate(s)).sum()
+        self.inputs
+            .iter()
+            .map(|s| Solution::evaluate(s, false))
+            .sum()
     }
-    fn evaluate(expression: &str) -> u64 {
+    fn solve_2(&self) -> u64 {
+        self.inputs
+            .iter()
+            .map(|s| Solution::evaluate(s, true))
+            .sum()
+    }
+    fn evaluate(expression: &str, advanced: bool) -> u64 {
         let v: &[u8] = expression.as_bytes();
-        let (mut op, mut n) = (b'+', 0);
-        let mut ret = 0;
+        let mut stack: Vec<(Op, u64)> = Vec::new();
+        let mut op = Op::Mul;
         let mut i = 0;
         while i < v.len() {
             match v[i] {
-                b'0'..=b'9' => n = n * 10 + (v[i] - b'0') as u64,
-                b'+' | b'*' => {
-                    ret = if op == b'+' { ret + n } else { ret * n };
-                    n = 0;
-                    op = v[i];
-                }
+                b'0'..=b'9' => stack.push((op, (v[i] - b'0') as u64)),
+                b'+' => op = Op::Add,
+                b'*' => op = Op::Mul,
                 b'(' => {
                     let (mut j, mut depth) = (i + 1, 1);
                     while depth > 0 {
@@ -34,17 +46,31 @@ impl Solution {
                         };
                         j += 1;
                     }
-                    n = Solution::evaluate(&expression[i + 1..j - 1]);
+                    stack.push((op, Solution::evaluate(&expression[i + 1..j - 1], advanced)));
                     i = j;
                 }
                 _ => {}
             }
             i += 1;
         }
-        if op == b'+' {
-            ret + n
+        if advanced {
+            let mut ret = 1;
+            while let Some(last) = stack.pop() {
+                match last.0 {
+                    Op::Add => {
+                        if let Some(prev) = stack.last_mut() {
+                            prev.1 += last.1;
+                        }
+                    }
+                    Op::Mul => ret *= last.1,
+                }
+            }
+            ret
         } else {
-            ret * n
+            stack.iter().fold(1, |acc, x| match x.0 {
+                Op::Add => acc + x.1,
+                Op::Mul => acc * x.1,
+            })
         }
     }
 }
@@ -57,6 +83,7 @@ fn main() {
             .collect(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -65,17 +92,39 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(71, Solution::evaluate("1 + 2 * 3 + 4 * 5 + 6"));
-        assert_eq!(51, Solution::evaluate("1 + (2 * 3) + (4 * (5 + 6))"));
-        assert_eq!(26, Solution::evaluate("2 * 3 + (4 * 5)"));
-        assert_eq!(437, Solution::evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        assert_eq!(71, Solution::evaluate("1 + 2 * 3 + 4 * 5 + 6", false));
+        assert_eq!(51, Solution::evaluate("1 + (2 * 3) + (4 * (5 + 6))", false));
+        assert_eq!(26, Solution::evaluate("2 * 3 + (4 * 5)", false));
+        assert_eq!(
+            437,
+            Solution::evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", false)
+        );
         assert_eq!(
             12240,
-            Solution::evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+            Solution::evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", false)
         );
         assert_eq!(
             13632,
-            Solution::evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+            Solution::evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", false)
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(231, Solution::evaluate("1 + 2 * 3 + 4 * 5 + 6", true));
+        assert_eq!(51, Solution::evaluate("1 + (2 * 3) + (4 * (5 + 6))", true));
+        assert_eq!(46, Solution::evaluate("2 * 3 + (4 * 5)", true));
+        assert_eq!(
+            1445,
+            Solution::evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", true)
+        );
+        assert_eq!(
+            669060,
+            Solution::evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", true)
+        );
+        assert_eq!(
+            23340,
+            Solution::evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", true)
         );
     }
 }
