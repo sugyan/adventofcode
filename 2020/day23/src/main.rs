513fc802047228b82635964ca5fa2f11fb732be3
sugyan <sugi1982@gmail.com>
2020-12-24 02:39:38 +0900

    Update 2020/day23

--- 2020/day23/src/main.rs
+++ 2020/day23/src/main.rs
@@ -1,34 +1,94 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 
 struct Solution {
-    cups: Vec<u8>,
+    cups: Vec<u64>,
 }
 
 impl Solution {
     fn new(input: String) -> Self {
         Self {
-            cups: input.as_bytes().iter().map(|&b| b - b'0').collect(),
+            cups: input
+                .as_bytes()
+                .iter()
+                .map(|&b| (b - b'0') as u64)
+                .collect(),
         }
     }
     fn solve_1(&self) -> String {
-        let mut cups = self.cups.clone();
-        for _ in 0..100 {
-            let current = cups.remove(0);
-            let pickup: Vec<u8> = (0..3).map(|_| cups.remove(0)).collect();
-            let mut destination = if current == 1 { 9 } else { current - 1 };
-            while pickup.contains(&destination) {
-                destination = if destination == 1 { 9 } else { destination - 1 };
+        let cups = self.game(9, 100);
+        let mut ret = Vec::new();
+        let mut cup = 1;
+        for _ in 0..8 {
+            if let Some(&next) = cups.get(&cup) {
+                ret.push(next.to_string());
+                cup = next;
             }
-            if let Some(p) = cups.iter().position(|&x| x == destination) {
-                cups.splice(p + 1..p + 1, pickup);
+        }
+        ret.concat()
+    }
+    fn solve_2(&self) -> u64 {
+        let cups = self.game(1_000_000, 10_000_000);
+        let mut ret = 1;
+        let mut cup = 1;
+        for _ in 0..2 {
+            if let Some(&next) = cups.get(&cup) {
+                ret *= next;
+                cup = next;
             }
-            cups.push(current);
         }
-        while cups[0] != 1 {
-            let first = cups.remove(0);
-            cups.push(first);
+        ret
+    }
+    fn game(&self, cups: usize, moves: usize) -> HashMap<u64, u64> {
+        let mut hm = HashMap::with_capacity(cups);
+        let mut last = None;
+        for (i, &cup) in self.cups.iter().enumerate() {
+            if i > 0 {
+                hm.insert(self.cups[i - 1], cup);
+            }
+            last = Some(cup);
         }
-        cups.iter().skip(1).map(|&u| (u + b'0') as char).collect()
+        for i in self.cups.len()..cups {
+            if let Some(l) = last {
+                hm.insert(l, i as u64 + 1);
+            }
+            last = Some(i as u64 + 1)
+        }
+        if let Some(l) = last {
+            hm.insert(l, self.cups[0]);
+        }
+
+        let highest = cups as u64;
+        let mut current = self.cups[0];
+        let mut pickups = [0; 3];
+        for _ in 0..moves {
+            let mut p = current;
+            for pickup in pickups.iter_mut() {
+                if let Some(&next) = hm.get(&p) {
+                    p = next;
+                }
+                *pickup = p;
+            }
+            let mut destination = if current > 1 { current - 1 } else { highest };
+            while pickups.iter().any(|&x| x == destination) {
+                destination = if destination > 1 {
+                    destination - 1
+                } else {
+                    highest
+                };
+            }
+            if let (Some(&c_next), Some(&p_next), Some(&d_next)) =
+                (hm.get(&current), hm.get(&p), hm.get(&destination))
+            {
+                hm.insert(current, p_next);
+                hm.insert(p, d_next);
+                hm.insert(destination, c_next);
+            }
+            if let Some(&next) = hm.get(&current) {
+                current = next;
+            }
+        }
+        hm
     }
 }
 
@@ -41,6 +101,7 @@ fn main() {
             .unwrap(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -52,6 +113,14 @@ mod tests {
         assert_eq!(
             "67384529",
             Solution::new(String::from("389125467")).solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            149245887792,
+            Solution::new(String::from("389125467")).solve_2()
         );
     }
 }
