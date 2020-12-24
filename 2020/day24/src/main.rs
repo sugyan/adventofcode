6395e2372bf7ffcfeba2a846085a6412058eb3d1
sugyan <sugi1982@gmail.com>
2020-12-24 17:38:59 +0900

    Add 2020/day24 part2

--- 2020/day24/src/main.rs
+++ 2020/day24/src/main.rs
@@ -2,16 +2,13 @@ use std::collections::HashSet;
 use std::io::{BufRead, BufReader};
 
 struct Solution {
-    inputs: Vec<String>,
+    flipped: HashSet<(i32, i32)>,
 }
 
 impl Solution {
     fn new(inputs: Vec<String>) -> Self {
-        Self { inputs }
-    }
-    fn solve_1(&self) -> usize {
         let mut flipped = HashSet::new();
-        for input in self.inputs.iter() {
+        for input in inputs.iter() {
             let s: Vec<char> = input.chars().collect();
             let mut p = (0, 0);
             let mut i = 0;
@@ -47,6 +44,33 @@ impl Solution {
                 flipped.insert(p);
             }
         }
+        Self { flipped }
+    }
+    fn solve_1(&self) -> usize {
+        self.flipped.len()
+    }
+    fn solve_2(&self) -> usize {
+        let mut flipped = self.flipped.clone();
+        let adjacents = [(2, 0), (1, -1), (-1, -1), (-2, 0), (-1, 1), (1, 1)];
+        for _ in 0..100 {
+            let mut targets = HashSet::new();
+            flipped.iter().for_each(|&p| {
+                targets.insert(p);
+                adjacents.iter().for_each(|&d| {
+                    targets.insert((p.0 + d.0, p.1 + d.1));
+                })
+            });
+            flipped = targets
+                .into_iter()
+                .filter(|&p| {
+                    let count = adjacents
+                        .iter()
+                        .filter(|&d| flipped.contains(&(p.0 + d.0, p.1 + d.1)))
+                        .count();
+                    count == 2 || (count == 1 && flipped.contains(&p))
+                })
+                .collect();
+        }
         flipped.len()
     }
 }
@@ -59,6 +83,7 @@ fn main() {
             .collect(),
     );
     println!("{}", solution.solve_1());
+    println!("{}", solution.solve_2());
 }
 
 #[cfg(test)]
@@ -96,6 +121,40 @@ wseweeenwnesenwwwswnew"[1..]
                     .collect()
             )
             .solve_1()
+        );
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(
+            2208,
+            Solution::new(
+                "
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"[1..]
+                    .split('\n')
+                    .map(str::to_string)
+                    .collect()
+            )
+            .solve_2()
         );
     }
 }
