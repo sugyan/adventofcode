4b4d2cbbb0b71aa39c64e0e9bc6288e23154ca36
sugyan <sugi1982@gmail.com>
2021-02-04 00:22:49 +0900

    Add 2020/python/day22,day24

---
+++ 2020/python/day24.py
@@ -0,0 +1,57 @@
+import sys
+from copy import deepcopy
+from operator import add
+from typing import List, Set, Tuple
+
+
+class Solution:
+    def __init__(self, inputs: List[str]) -> None:
+        def position(s: str) -> Tuple[int, ...]:
+            p = [0, 0]
+            ns = False
+            for c in s:
+                if c == "e":
+                    p[0] += 1 if ns else 2
+                    ns = False
+                if c == "s":
+                    p[1] -= 1
+                    ns = True
+                if c == "w":
+                    p[0] -= 1 if ns else 2
+                    ns = False
+                if c == "n":
+                    p[1] += 1
+                    ns = True
+            return tuple(p)
+
+        self.flipped: Set[Tuple[int, ...]] = set()
+        for line in inputs:
+            pos = position(line)
+            if pos in self.flipped:
+                self.flipped.remove(pos)
+            else:
+                self.flipped.add(pos)
+
+    def part_1(self) -> int:
+        return len(self.flipped)
+
+    def part_2(self) -> int:
+        neighbors = [[2, 0], [1, -1], [-1, -1], [-2, 0], [-1, 1], [1, 1]]
+        flipped = deepcopy(self.flipped)
+
+        def flip(p: Tuple[int, ...]) -> bool:
+            count = sum([tuple(map(add, p, d)) in flipped for d in neighbors])
+            return count == 2 or (count == 1 and p in flipped)
+
+        for _ in range(100):
+            candidates = set(flipped)
+            for p in flipped:
+                candidates.update([(tuple(map(add, p, d))) for d in neighbors])
+            flipped = set(filter(flip, candidates))
+        return len(flipped)
+
+
+if __name__ == "__main__":
+    solution = Solution(sys.stdin.read().splitlines())
+    print(f"Part 1: {solution.part_1()}")
+    print(f"Part 2: {solution.part_2()}")
---
+++ 2020/python/tests/test_day24.py
@@ -0,0 +1,35 @@
+import unittest
+from typing import List
+
+from day24 import Solution
+
+
+class TestDay24(unittest.TestCase):
+    def example_inputs(self) -> List[str]:
+        return """\
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew""".splitlines()
+
+    def test_part_1(self) -> None:
+        self.assertEqual(10, Solution(self.example_inputs()).part_1())
+
+    def test_part_2(self) -> None:
+        self.assertEqual(2208, Solution(self.example_inputs()).part_2())
