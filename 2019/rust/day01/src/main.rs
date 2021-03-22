37676fd70029113515450a45a9b02bd5dc23e2e6
sugyan <sugi1982@gmail.com>
2021-03-23 00:01:18 +0900

    Add 2019/rust/day01

---
+++ 2019/rust/day01/src/main.rs
@@ -0,0 +1,60 @@
+use std::io::{BufRead, BufReader};
+
+struct Solution {
+    modules: Vec<i32>,
+}
+
+impl Solution {
+    fn new(inputs: &[String]) -> Self {
+        Self {
+            modules: inputs.iter().filter_map(|s| s.parse().ok()).collect(),
+        }
+    }
+    fn part_1(&self) -> i32 {
+        self.modules.iter().map(|&mass| mass / 3 - 2).sum()
+    }
+    fn part_2(&self) -> i32 {
+        self.modules
+            .iter()
+            .map(|&mass| {
+                (0..)
+                    .scan(mass, |state, _| {
+                        *state = *state / 3 - 2;
+                        Some(*state).filter(|&m| m >= 0)
+                    })
+                    .sum::<i32>()
+            })
+            .sum()
+    }
+}
+
+fn main() {
+    let solution = Solution::new(
+        &BufReader::new(std::io::stdin().lock())
+            .lines()
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>(),
+    );
+    println!("Part 1: {}", solution.part_1());
+    println!("Part 2: {}", solution.part_2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(2, Solution::new(&[String::from("12")]).part_1());
+        assert_eq!(2, Solution::new(&[String::from("14")]).part_1());
+        assert_eq!(654, Solution::new(&[String::from("1969")]).part_1());
+        assert_eq!(33583, Solution::new(&[String::from("100756")]).part_1());
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(2, Solution::new(&[String::from("14")]).part_2());
+        assert_eq!(966, Solution::new(&[String::from("1969")]).part_2());
+        assert_eq!(50346, Solution::new(&[String::from("100756")]).part_2());
+    }
+}
