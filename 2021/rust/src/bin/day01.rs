3867e401b7bb846b45bae0a233376ca36bf145d9
sugyan <sugi1982@gmail.com>
2021-12-01 14:21:42 +0900

    Add 2021/rust/day01 part2

--- 2021/rust/src/bin/day01.rs
+++ 2021/rust/src/bin/day01.rs
@@ -10,8 +10,17 @@ impl Solution {
             reports: inputs.iter().filter_map(|s| s.parse().ok()).collect(),
         }
     }
-    fn part_1(self) -> usize {
+    fn part_1(&self) -> usize {
         self.reports.windows(2).filter(|w| w[1] > w[0]).count()
+    }
+    fn part_2(&self) -> usize {
+        self.reports
+            .windows(3)
+            .map(|w| w[0] + w[1] + w[2])
+            .collect::<Vec<_>>()
+            .windows(2)
+            .filter(|w| w[1] > w[0])
+            .count()
     }
 }
 
@@ -23,19 +32,15 @@ fn main() {
             .collect::<Vec<_>>(),
     );
     println!("Part 1: {}", solution.part_1());
-    // println!("Part 2: {}", solution.part_2());
+    println!("Part 2: {}", solution.part_2());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn example_1() {
-        assert_eq!(
-            7,
-            Solution::new(
-                &r"
+    fn example_inputs() -> Vec<String> {
+        r"
 199
 200
 208
@@ -46,11 +51,18 @@ mod tests {
 269
 260
 263"[1..]
-                    .split('\n')
-                    .map(String::from)
-                    .collect::<Vec<_>>()
-            )
-            .part_1()
-        );
+            .split('\n')
+            .map(String::from)
+            .collect::<Vec<_>>()
+    }
+
+    #[test]
+    fn example_1() {
+        assert_eq!(7, Solution::new(&example_inputs()).part_1());
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(5, Solution::new(&example_inputs()).part_2());
     }
 }
