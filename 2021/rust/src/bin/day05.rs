e1511b11196672dbaa2a3c3ccc5c2ee7075e4c46
sugyan <sugi1982@gmail.com>
2021-12-05 14:57:29 +0900

    Add 2021/rust/day05 part2

--- 2021/rust/src/bin/day05.rs
+++ 2021/rust/src/bin/day05.rs
@@ -24,19 +24,7 @@ impl Solution {
         }
     }
     fn part_1(&self) -> usize {
-        let xmax = self
-            .lines
-            .iter()
-            .map(|((x1, _), (x2, _))| x1.max(x2))
-            .max()
-            .unwrap();
-        let ymax = self
-            .lines
-            .iter()
-            .map(|((_, y1), (_, y2))| y1.max(y2))
-            .max()
-            .unwrap();
-        let mut grid = vec![vec![0; xmax + 1]; ymax + 1];
+        let mut grid = self.make_grid();
         for &((x1, y1), (x2, y2)) in &self.lines {
             if x1 == x2 {
                 for y in y1.min(y2)..=y1.max(y2) {
@@ -53,6 +41,48 @@ impl Solution {
             .map(|row| row.iter().filter(|&&v| v > 1).count())
             .sum()
     }
+    fn part_2(&self) -> usize {
+        let mut grid = self.make_grid();
+        for &((x1, y1), (x2, y2)) in &self.lines {
+            if x1 == x2 {
+                for y in y1.min(y2)..=y1.max(y2) {
+                    grid[y][x1] += 1;
+                }
+            }
+            if y1 == y2 {
+                for x in x1.min(x2)..=x1.max(x2) {
+                    grid[y1][x] += 1;
+                }
+            }
+            if x1.max(x2) - x1.min(x2) == y1.max(y2) - y1.min(y2) {
+                let d = (if x2 > x1 { 1 } else { !0 }, if y2 > y1 { 1 } else { !0 });
+                let mut xy = (x1, y1);
+                for _ in 0..=x1.max(x2) - x1.min(x2) {
+                    grid[xy.1][xy.0] += 1;
+                    xy.0 = xy.0.wrapping_add(d.0);
+                    xy.1 = xy.1.wrapping_add(d.1);
+                }
+            }
+        }
+        grid.iter()
+            .map(|row| row.iter().filter(|&&v| v > 1).count())
+            .sum()
+    }
+    fn make_grid(&self) -> Vec<Vec<u32>> {
+        let xmax = self
+            .lines
+            .iter()
+            .map(|((x1, _), (x2, _))| x1.max(x2))
+            .max()
+            .unwrap();
+        let ymax = self
+            .lines
+            .iter()
+            .map(|((_, y1), (_, y2))| y1.max(y2))
+            .max()
+            .unwrap();
+        vec![vec![0; xmax + 1]; ymax + 1]
+    }
 }
 
 fn main() {
@@ -63,6 +93,7 @@ fn main() {
             .collect::<Vec<_>>(),
     );
     println!("{}", solution.part_1());
+    println!("{}", solution.part_2());
 }
 
 #[cfg(test)]
@@ -89,5 +120,10 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(5, Solution::new(&example_inputs()).part_1());
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(12, Solution::new(&example_inputs()).part_2());
     }
 }
