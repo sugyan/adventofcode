a1347c3ac18583085b45ad2a11cd9e5307e9cfa7
sugyan <sugi1982@gmail.com>
2021-12-09 14:45:00 +0900

    Add 2021/rust/day09 part2

--- 2021/rust/src/bin/day09.rs
+++ 2021/rust/src/bin/day09.rs
@@ -1,40 +1,71 @@
+use std::collections::VecDeque;
 use std::io::{BufRead, BufReader};
 
 struct Solution {
     heightmap: Vec<Vec<u8>>,
+    low_points: Vec<(usize, usize)>,
 }
 
 impl Solution {
     fn new(inputs: &[String]) -> Self {
+        let heightmap = inputs
+            .iter()
+            .map(|line| line.bytes().map(|u| u - b'0').collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let (r, c) = (heightmap.len(), heightmap[0].len());
+        let mut low_points = Vec::new();
+        for (i, row) in heightmap.iter().enumerate() {
+            for (j, &h) in row.iter().enumerate() {
+                if [0, 1, 0, !0, 0].windows(2).all(|d| {
+                    let i = i.wrapping_add(d[0]);
+                    let j = j.wrapping_add(d[1]);
+                    !(0..r).contains(&i) || !(0..c).contains(&j) || heightmap[i][j] > h
+                }) {
+                    low_points.push((i, j));
+                }
+            }
+        }
         Self {
-            heightmap: inputs
-                .iter()
-                .map(|line| line.bytes().map(|u| u - b'0').collect())
-                .collect(),
+            heightmap,
+            low_points,
         }
     }
     fn part_1(&self) -> u32 {
-        let (r, c) = (self.heightmap.len(), self.heightmap[0].len());
-        self.heightmap
+        self.low_points
             .iter()
-            .enumerate()
-            .map(|(i, row)| {
-                row.iter()
-                    .enumerate()
-                    .map(|(j, &h)| {
-                        if (i == 0 || self.heightmap[i - 1][j] > h)
-                            && (i == r - 1 || self.heightmap[i + 1][j] > h)
-                            && (j == 0 || self.heightmap[i][j - 1] > h)
-                            && (j == c - 1 || self.heightmap[i][j + 1] > h)
-                        {
-                            h as u32 + 1
-                        } else {
-                            0
-                        }
-                    })
-                    .sum::<u32>()
-            })
+            .map(|&(i, j)| self.heightmap[i][j] as u32 + 1)
             .sum()
+    }
+    fn part_2(&self) -> u32 {
+        let (r, c) = (self.heightmap.len(), self.heightmap[0].len());
+        let mut seen = vec![vec![false; self.heightmap[0].len()]; self.heightmap.len()];
+        let mut sizes = self
+            .low_points
+            .iter()
+            .map(|&(i, j)| {
+                let mut queue = VecDeque::new();
+                let mut size = 0;
+                queue.push_back((i, j));
+                while let Some((i, j)) = queue.pop_front() {
+                    for d in [0, 1, 0, !0, 0].windows(2) {
+                        let i = i.wrapping_add(d[0]);
+                        let j = j.wrapping_add(d[1]);
+                        if (0..r).contains(&i)
+                            && (0..c).contains(&j)
+                            && self.heightmap[i][j] < 9
+                            && !seen[i][j]
+                        {
+                            size += 1;
+                            seen[i][j] = true;
+                            queue.push_back((i, j));
+                        }
+                    }
+                }
+                size
+            })
+            .collect::<Vec<_>>();
+        sizes.sort_unstable();
+        sizes.iter().rev().take(3).product()
     }
 }
 
@@ -46,6 +77,7 @@ fn main() {
             .collect::<Vec<_>>(),
     );
     println!("{}", solution.part_1());
+    println!("{}", solution.part_2());
 }
 
 #[cfg(test)]
@@ -67,5 +99,10 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(15, Solution::new(&example_inputs()).part_1());
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(1134, Solution::new(&example_inputs()).part_2());
     }
 }
