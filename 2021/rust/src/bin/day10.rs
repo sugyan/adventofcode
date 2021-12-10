ce9ce5ffa9bf30de66ebafa4ea1159d82c63d4fb
sugyan <sugi1982@gmail.com>
2021-12-10 18:45:32 +0900

    Add 2021/rust/day10 part2

--- 2021/rust/src/bin/day10.rs
+++ 2021/rust/src/bin/day10.rs
@@ -1,44 +1,88 @@
 use std::io::{BufRead, BufReader};
 
+enum Score {
+    Incomplete(u64),
+    Corrupted(u64),
+}
+
 struct Solution {
-    lines: Vec<String>,
+    scores: Vec<Score>,
 }
 
 impl Solution {
     fn new(inputs: &[String]) -> Self {
         Self {
-            lines: inputs.to_vec(),
+            scores: inputs
+                .iter()
+                .map(|line| {
+                    let mut stack = Vec::new();
+                    for c in line.chars() {
+                        match c {
+                            ')' => {
+                                if stack.last() == Some(&'(') {
+                                    stack.pop();
+                                } else {
+                                    return Score::Corrupted(3);
+                                }
+                            }
+                            ']' => {
+                                if stack.last() == Some(&'[') {
+                                    stack.pop();
+                                } else {
+                                    return Score::Corrupted(57);
+                                }
+                            }
+                            '}' => {
+                                if stack.last() == Some(&'{') {
+                                    stack.pop();
+                                } else {
+                                    return Score::Corrupted(1197);
+                                }
+                            }
+                            '>' => {
+                                if stack.last() == Some(&'<') {
+                                    stack.pop();
+                                } else {
+                                    return Score::Corrupted(25137);
+                                }
+                            }
+                            c => stack.push(c),
+                        }
+                    }
+                    Score::Incomplete(stack.iter().rev().fold(0, |acc, &c| {
+                        acc * 5
+                            + match c {
+                                '(' => 1,
+                                '[' => 2,
+                                '{' => 3,
+                                '<' => 4,
+                                _ => unreachable!(),
+                            }
+                    }))
+                })
+                .collect(),
         }
     }
-    fn part_1(&self) -> u32 {
-        self.lines
+    fn part_1(&self) -> u64 {
+        self.scores
             .iter()
-            .map(|line| {
-                let mut stack = Vec::new();
-                for c in line.chars() {
-                    match c {
-                        ')' if stack.last() == Some(&'(') => {
-                            stack.pop();
-                        }
-                        ')' => return 3,
-                        ']' if stack.last() == Some(&'[') => {
-                            stack.pop();
-                        }
-                        ']' => return 57,
-                        '}' if stack.last() == Some(&'{') => {
-                            stack.pop();
-                        }
-                        '}' => return 1197,
-                        '>' if stack.last() == Some(&'<') => {
-                            stack.pop();
-                        }
-                        '>' => return 25137,
-                        c => stack.push(c),
-                    }
-                }
-                0
+            .filter_map(|s| match s {
+                Score::Corrupted(u) => Some(*u),
+                _ => None,
             })
             .sum()
+    }
+    fn part_2(&self) -> u64 {
+        let mut v = self
+            .scores
+            .iter()
+            .filter_map(|s| match s {
+                Score::Incomplete(u) => Some(*u),
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+        v.sort_unstable();
+        v[v.len() / 2]
     }
 }
 
@@ -50,6 +94,7 @@ fn main() {
             .collect::<Vec<_>>(),
     );
     println!("{}", solution.part_1());
+    println!("{}", solution.part_2());
 }
 
 #[cfg(test)]
@@ -76,5 +121,10 @@ mod tests {
     #[test]
     fn example_1() {
         assert_eq!(26397, Solution::new(&example_inputs()).part_1());
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(288957, Solution::new(&example_inputs()).part_2());
     }
 }
