79091bf60897516303d35314e20f057c3be23543
sugyan <sugi1982@gmail.com>
2022-11-22 23:38:54 +0900

    Update 2021/rust project

--- 2021/rust/src/bin/day01.rs
+++ 2021/rust/src/bin/day01.rs
@@ -1,21 +1,11 @@
-use std::io::{BufRead, BufReader};
+use aoc2022::Solve;
+use std::io::{BufRead, BufReader, Read};
 
 struct Solution {
     reports: Vec<u32>,
 }
 
 impl Solution {
-    fn new(inputs: &[String]) -> Self {
-        Self {
-            reports: inputs.iter().filter_map(|s| s.parse().ok()).collect(),
-        }
-    }
-    fn part_1(&self) -> usize {
-        self.count_increases(1)
-    }
-    fn part_2(&self) -> usize {
-        self.count_increases(3)
-    }
     fn count_increases(&self, size: usize) -> usize {
         (size..self.reports.len())
             .filter(|&i| self.reports[i] > self.reports[i - size])
@@ -23,22 +13,38 @@ impl Solution {
     }
 }
 
+impl Solve for Solution {
+    type Answer1 = usize;
+    type Answer2 = usize;
+
+    fn new(r: impl Read) -> Self {
+        Self {
+            reports: BufReader::new(r)
+                .lines()
+                .filter_map(Result::ok)
+                .filter_map(|s| s.parse().ok())
+                .collect::<Vec<_>>(),
+        }
+    }
+    fn part1(&self) -> Self::Answer1 {
+        self.count_increases(1)
+    }
+    fn part2(&self) -> Self::Answer2 {
+        self.count_increases(3)
+    }
+}
+
 fn main() {
-    let solution = Solution::new(
-        &BufReader::new(std::io::stdin().lock())
-            .lines()
-            .filter_map(Result::ok)
-            .collect::<Vec<_>>(),
-    );
-    println!("Part 1: {}", solution.part_1());
-    println!("Part 2: {}", solution.part_2());
+    let solution = Solution::new(std::io::stdin().lock());
+    println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_inputs() -> Vec<String> {
+    fn example_input() -> &'static [u8] {
         r"
 199
 200
@@ -50,18 +56,16 @@ mod tests {
 269
 260
 263"[1..]
-            .split('\n')
-            .map(String::from)
-            .collect()
+            .as_bytes()
     }
 
     #[test]
     fn example_1() {
-        assert_eq!(7, Solution::new(&example_inputs()).part_1());
+        assert_eq!(7, Solution::new(example_input()).part1());
     }
 
     #[test]
     fn example_2() {
-        assert_eq!(5, Solution::new(&example_inputs()).part_2());
+        assert_eq!(5, Solution::new(example_input()).part2());
     }
 }
