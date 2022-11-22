4801232b91827a1e53255b33a06e2799fb12604b
sugyan <sugi1982@gmail.com>
2022-11-23 00:02:09 +0900

    Update 2021/rust/day02

--- 2021/rust/src/bin/day02.rs
+++ 2021/rust/src/bin/day02.rs
@@ -1,4 +1,5 @@
-use std::io::{BufRead, BufReader};
+use aoc2022::Solve;
+use std::io::{BufRead, BufReader, Read};
 
 enum Command {
     Forward(u32),
@@ -6,13 +7,31 @@ enum Command {
     Up(u32),
 }
 
+#[derive(Default)]
+struct Position(u32, u32, u32);
+
 struct Solution {
     commands: Vec<Command>,
 }
 
 impl Solution {
-    fn new(inputs: &[String]) -> Self {
-        let parse = |s: &String| {
+    fn calculate_position(&self) -> Position {
+        self.commands
+            .iter()
+            .fold(Position::default(), |acc, c| match c {
+                Command::Forward(u) => Position(acc.0 + u, acc.1 + u * acc.2, acc.2),
+                Command::Down(u) => Position(acc.0, acc.1, acc.2 + u),
+                Command::Up(u) => Position(acc.0, acc.1, acc.2 - u),
+            })
+    }
+}
+
+impl Solve for Solution {
+    type Answer1 = u32;
+    type Answer2 = u32;
+
+    fn new(r: impl Read) -> Self {
+        let parse = |s: String| {
             let i = s.find(' ').unwrap();
             let units = s[i + 1..].parse().unwrap();
             match &s[..i] {
@@ -23,42 +42,34 @@ impl Solution {
             }
         };
         Self {
-            commands: inputs.iter().map(parse).collect(),
+            commands: BufReader::new(r)
+                .lines()
+                .filter_map(Result::ok)
+                .map(parse)
+                .collect(),
         }
     }
-    fn part_1(&self) -> u32 {
+    fn part1(&self) -> u32 {
         let position = self.calculate_position();
         position.0 * position.2
     }
-    fn part_2(&self) -> u32 {
+    fn part2(&self) -> u32 {
         let position = self.calculate_position();
         position.0 * position.1
-    }
-    fn calculate_position(&self) -> (u32, u32, u32) {
-        self.commands.iter().fold((0, 0, 0), |acc, c| match c {
-            Command::Forward(u) => (acc.0 + u, acc.1 + u * acc.2, acc.2),
-            Command::Down(n) => (acc.0, acc.1, acc.2 + n),
-            Command::Up(n) => (acc.0, acc.1, acc.2 - n),
-        })
     }
 }
 
 fn main() {
-    let solution = Solution::new(
-        &BufReader::new(std::io::stdin().lock())
-            .lines()
-            .filter_map(Result::ok)
-            .collect::<Vec<_>>(),
-    );
-    println!("{}", solution.part_1());
-    println!("{}", solution.part_2());
+    let solution = Solution::new(std::io::stdin().lock());
+    println!("{}", solution.part1());
+    println!("{}", solution.part2());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_inputs() -> Vec<String> {
+    fn example_input() -> &'static [u8] {
         r"
 forward 5
 down 5
@@ -66,18 +77,16 @@ forward 8
 up 3
 down 8
 forward 2"[1..]
-            .split('\n')
-            .map(String::from)
-            .collect()
+            .as_bytes()
     }
 
     #[test]
     fn example_1() {
-        assert_eq!(150, Solution::new(&example_inputs()).part_1());
+        assert_eq!(150, Solution::new(example_input()).part1());
     }
 
     #[test]
     fn example_2() {
-        assert_eq!(900, Solution::new(&example_inputs()).part_2());
+        assert_eq!(900, Solution::new(example_input()).part2());
     }
 }
