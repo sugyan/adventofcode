4f9f33cef183c38081ed6d9ff852970594672599
sugyan <sugi1982@gmail.com>
2022-12-01 14:25:19 +0900

    Add 2022/rust/day01 part2

--- 2022/rust/src/bin/day01.rs
+++ 2022/rust/src/bin/day01.rs
@@ -2,7 +2,13 @@ use aoc2022::Solve;
 use std::io::{BufRead, BufReader};
 
 struct Solution {
-    calories: Vec<Vec<u32>>,
+    total_calories: Vec<u32>,
+}
+
+impl Solution {
+    fn top_sum(&self, n: usize) -> u32 {
+        self.total_calories.iter().rev().take(n).sum()
+    }
 }
 
 impl Solve for Solution {
@@ -10,21 +16,26 @@ impl Solve for Solution {
     type Answer2 = u32;
 
     fn new(r: impl std::io::Read) -> Self {
-        Self {
-            calories: BufReader::new(r)
-                .lines()
-                .filter_map(Result::ok)
-                .collect::<Vec<_>>()
-                .split(String::is_empty)
-                .map(|lines| lines.iter().filter_map(|line| line.parse().ok()).collect())
-                .collect(),
-        }
+        let mut total_calories = BufReader::new(r)
+            .lines()
+            .filter_map(Result::ok)
+            .collect::<Vec<_>>()
+            .split(String::is_empty)
+            .map(|lines| {
+                lines
+                    .iter()
+                    .filter_map(|line| line.parse::<u32>().ok())
+                    .sum()
+            })
+            .collect::<Vec<_>>();
+        total_calories.sort_unstable();
+        Self { total_calories }
     }
     fn part1(&self) -> Self::Answer1 {
-        self.calories.iter().map(|v| v.iter().sum()).max().unwrap()
+        self.top_sum(1)
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.top_sum(3)
     }
 }
 
@@ -60,5 +71,10 @@ mod tests {
     #[test]
     fn example1() {
         assert_eq!(24000, Solution::new(example_input()).part1());
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(45000, Solution::new(example_input()).part2());
     }
 }
