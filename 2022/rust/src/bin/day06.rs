808bad291edc089e6d22af519b3a0698dc136895
sugyan <sugi1982@gmail.com>
2022-12-06 15:47:32 +0900

    Add 2022/rust/day06 part2

--- 2022/rust/src/bin/day06.rs
+++ 2022/rust/src/bin/day06.rs
@@ -5,31 +5,32 @@ struct Solution {
     datastream: Vec<u8>,
 }
 
+impl Solution {
+    fn marker_detected_position(&self, window_size: usize) -> usize {
+        self.datastream
+            .windows(window_size)
+            .position(|w| {
+                w.iter().fold(0_u128, |acc, x| acc | 1 << x).count_ones() as usize == window_size
+            })
+            .unwrap()
+            + window_size
+    }
+}
+
 impl Solve for Solution {
     type Answer1 = usize;
     type Answer2 = usize;
 
     fn new(r: impl Read) -> Self {
-        let mut buf = String::new();
-        BufReader::new(r).read_to_string(&mut buf).unwrap();
-        Self {
-            datastream: buf.bytes().collect(),
-        }
+        let mut datastream = Vec::new();
+        BufReader::new(r).read_to_end(&mut datastream).unwrap();
+        Self { datastream }
     }
     fn part1(&self) -> Self::Answer1 {
-        self.datastream
-            .windows(4)
-            .position(|w| {
-                w.iter()
-                    .fold(0_u32, |acc, x| acc | 1 << (x - b'a'))
-                    .count_ones()
-                    == 4
-            })
-            .unwrap()
-            + 4
+        self.marker_detected_position(4)
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.marker_detected_position(14)
     }
 }
 
@@ -64,6 +65,30 @@ mod tests {
         assert_eq!(
             11,
             Solution::new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes()).part1()
+        );
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(
+            19,
+            Solution::new("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes()).part2()
+        );
+        assert_eq!(
+            23,
+            Solution::new("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes()).part2()
+        );
+        assert_eq!(
+            23,
+            Solution::new("nppdvjthqldpwncqszvftbrmjlhg".as_bytes()).part2()
+        );
+        assert_eq!(
+            29,
+            Solution::new("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes()).part2()
+        );
+        assert_eq!(
+            26,
+            Solution::new("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes()).part2()
         );
     }
 }
