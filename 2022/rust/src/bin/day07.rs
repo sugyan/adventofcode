48363952e974b94c4ebc8ba6e043926171d7d525
sugyan <sugi1982@gmail.com>
2022-12-07 16:53:12 +0900

    Update 2022/rust/day07

--- 2022/rust/src/bin/day07.rs
+++ 2022/rust/src/bin/day07.rs
@@ -1,60 +1,8 @@
 use aoc2022::Solve;
 use std::io::{BufRead, BufReader, Read};
 
-#[derive(Debug)]
-enum Entry {
-    File(u32),
-    Directory(Vec<Entry>),
-}
-
-#[derive(Debug)]
-struct FileSystem {
-    root: Entry,
-}
-
 struct Solution {
-    filesystem: FileSystem,
-}
-
-impl Solution {
-    fn browse_around(curr: &mut Vec<Entry>, lines: &mut impl Iterator<Item = String>) {
-        while let Some(line) = lines.next() {
-            let parts = line.split_ascii_whitespace().collect::<Vec<_>>();
-            match *parts.first().unwrap() {
-                "$" => {
-                    if parts[1] == "cd" {
-                        match *parts.get(2).unwrap() {
-                            ".." => return,
-                            "/" => {}
-                            _ => {
-                                let mut dir = Vec::new();
-                                Self::browse_around(&mut dir, lines);
-                                curr.push(Entry::Directory(dir));
-                            }
-                        }
-                    }
-                }
-                s => {
-                    if let Ok(size) = s.parse() {
-                        curr.push(Entry::File(size));
-                    }
-                }
-            }
-        }
-    }
-    fn traverse(curr: &Entry, totals: &mut Vec<u32>) -> u32 {
-        let mut total = 0;
-        match curr {
-            Entry::File(size) => return *size,
-            Entry::Directory(dir) => {
-                for entry in dir {
-                    total += Self::traverse(entry, totals);
-                }
-            }
-        }
-        totals.push(total);
-        total
-    }
+    total_sizes: Vec<u32>,
 }
 
 impl Solve for Solution {
@@ -62,27 +10,43 @@ impl Solve for Solution {
     type Answer2 = u32;
 
     fn new(r: impl Read) -> Self {
-        let mut root_dir = Vec::new();
-        Self::browse_around(
-            &mut root_dir,
-            &mut BufReader::new(r).lines().filter_map(Result::ok),
-        );
-        Self {
-            filesystem: FileSystem {
-                root: Entry::Directory(root_dir),
-            },
+        let mut stack = Vec::new();
+        let mut total_sizes = Vec::new();
+        for line in BufReader::new(r).lines().filter_map(Result::ok) {
+            let parts = line.split_ascii_whitespace().collect::<Vec<_>>();
+            if parts[1] == "cd" {
+                if parts[2] == ".." {
+                    if let Some(total) = stack.pop() {
+                        total_sizes.push(total);
+                        if let Some(last) = stack.last_mut() {
+                            *last += total;
+                        }
+                    }
+                } else {
+                    stack.push(0);
+                }
+            } else if let Ok(size) = parts[0].parse::<u32>() {
+                if let Some(last) = stack.last_mut() {
+                    *last += size;
+                }
+            }
         }
+        while let Some(total) = stack.pop() {
+            total_sizes.push(total);
+            if let Some(last) = stack.last_mut() {
+                *last += total;
+            }
+        }
+        Self { total_sizes }
     }
     fn part1(&self) -> Self::Answer1 {
-        let mut v = Vec::new();
-        Self::traverse(&self.filesystem.root, &mut v);
-        v.iter().filter(|&&x| x <= 100_000).sum()
+        self.total_sizes.iter().filter(|&x| *x <= 100_000).sum()
     }
     fn part2(&self) -> Self::Answer2 {
-        let mut v = Vec::new();
-        Self::traverse(&self.filesystem.root, &mut v);
-        *v.iter()
-            .filter(|&&x| v[v.len() - 1] - x < 40_000_000)
+        *self
+            .total_sizes
+            .iter()
+            .filter(|&x| self.total_sizes[self.total_sizes.len() - 1] - x < 40_000_000)
             .min()
             .unwrap()
     }
