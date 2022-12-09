2fbf45fee9f71925a4eb4b34f6ca85253697794b
sugyan <sugi1982@gmail.com>
2022-12-09 15:16:28 +0900

    Add 2022/rust/day09 part2

--- 2022/rust/src/bin/day09.rs
+++ 2022/rust/src/bin/day09.rs
@@ -27,6 +27,34 @@ struct Solution {
     motions: Vec<Motion>,
 }
 
+impl Solution {
+    fn tail_visited(&self, knots_count: usize) -> usize {
+        let mut knots = vec![(0_i32, 0_i32); knots_count];
+        let mut hs = HashSet::new();
+        for motion in &self.motions {
+            let (d, n) = match motion {
+                Motion::R(n) => ((1, 0), n),
+                Motion::U(n) => ((0, 1), n),
+                Motion::L(n) => ((-1, 0), n),
+                Motion::D(n) => ((0, -1), n),
+            };
+            for _ in 0..*n {
+                knots[0].0 += d.0;
+                knots[0].1 += d.1;
+                for i in 0..knots_count - 1 {
+                    let diff = (knots[i].0 - knots[i + 1].0, knots[i].1 - knots[i + 1].1);
+                    if diff.0.abs() > 1 || diff.1.abs() > 1 {
+                        knots[i + 1].0 += diff.0.signum();
+                        knots[i + 1].1 += diff.1.signum();
+                    }
+                }
+                hs.insert(knots[knots_count - 1]);
+            }
+        }
+        hs.len()
+    }
+}
+
 impl Solve for Solution {
     type Answer1 = usize;
     type Answer2 = usize;
@@ -41,30 +69,10 @@ impl Solve for Solution {
         }
     }
     fn part1(&self) -> Self::Answer1 {
-        let (mut head, mut tail) = ((0_i32, 0_i32), (0_i32, 0_i32));
-        let mut hs = HashSet::from([tail]);
-        for motion in &self.motions {
-            let (d, n) = match motion {
-                Motion::R(n) => ((1, 0), n),
-                Motion::U(n) => ((0, 1), n),
-                Motion::L(n) => ((-1, 0), n),
-                Motion::D(n) => ((0, -1), n),
-            };
-            for _ in 0..*n {
-                head.0 += d.0;
-                head.1 += d.1;
-                let diff = (head.0 - tail.0, head.1 - tail.1);
-                if diff.0.abs() > 1 || diff.1.abs() > 1 {
-                    tail.0 += diff.0.signum();
-                    tail.1 += diff.1.signum();
-                }
-                hs.insert(tail);
-            }
-        }
-        hs.len()
+        self.tail_visited(2)
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.tail_visited(10)
     }
 }
 
@@ -92,8 +100,28 @@ R 2
             .as_bytes()
     }
 
+    fn example_input_large() -> &'static [u8] {
+        r"
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"[1..]
+            .as_bytes()
+    }
+
     #[test]
     fn part1() {
         assert_eq!(13, Solution::new(example_input()).part1());
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(1, Solution::new(example_input()).part2());
+        assert_eq!(36, Solution::new(example_input_large()).part2());
     }
 }
