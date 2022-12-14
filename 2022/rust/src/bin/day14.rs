20561e6673026a416534b18587e609a8adb0ba06
sugyan <sugi1982@gmail.com>
2022-12-14 16:08:56 +0900

    Add 2022/rust/day14 part2

--- 2022/rust/src/bin/day14.rs
+++ 2022/rust/src/bin/day14.rs
@@ -66,13 +66,50 @@ impl Solve for Solution {
         unreachable!();
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        let mut bts = BTreeSet::new();
+        for path in &self.paths {
+            for w in path.windows(2) {
+                if w[0].0 == w[1].0 {
+                    for y in w[0].1.min(w[1].1)..=w[0].1.max(w[1].1) {
+                        bts.insert((y, w[0].0));
+                    }
+                }
+                if w[0].1 == w[1].1 {
+                    for x in w[0].0.min(w[1].0)..=w[0].0.max(w[1].0) {
+                        bts.insert((w[0].1, x));
+                    }
+                }
+            }
+        }
+        let ymax = bts.iter().last().unwrap().0;
+        for i in 0.. {
+            let mut sand = (500, 0);
+            if bts.contains(&(sand.1, sand.0)) {
+                return i;
+            }
+            while sand.1 < ymax + 1 {
+                if !bts.contains(&(sand.1 + 1, sand.0)) {
+                    sand.1 += 1;
+                } else if !bts.contains(&(sand.1 + 1, sand.0 - 1)) {
+                    sand.1 += 1;
+                    sand.0 -= 1;
+                } else if !bts.contains(&(sand.1 + 1, sand.0 + 1)) {
+                    sand.1 += 1;
+                    sand.0 += 1;
+                } else {
+                    break;
+                }
+            }
+            bts.insert((sand.1, sand.0));
+        }
+        unreachable!();
     }
 }
 
 fn main() {
     let solution = Solution::new(std::io::stdin().lock());
     println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
 }
 
 #[cfg(test)]
@@ -90,5 +127,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(24, Solution::new(example_input()).part1());
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(93, Solution::new(example_input()).part2());
     }
 }
