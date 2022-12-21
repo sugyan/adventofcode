a3593dbf0d2afdceecf308c6e047a70117468d55
sugyan <sugi1982@gmail.com>
2022-12-21 15:16:51 +0900

    Add 2022/rust/day21 part2

--- 2022/rust/src/bin/day21.rs
+++ 2022/rust/src/bin/day21.rs
@@ -4,7 +4,6 @@ use std::io::{BufRead, BufReader, Read};
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-#[derive(Debug)]
 enum Job {
     Number(i64),
     Add(String, String),
@@ -28,6 +27,15 @@ impl FromStr for Job {
     }
 }
 
+enum Expression {
+    Human,
+    Number(i64),
+    Add(Box<Expression>, Box<Expression>),
+    Sub(Box<Expression>, Box<Expression>),
+    Mul(Box<Expression>, Box<Expression>),
+    Div(Box<Expression>, Box<Expression>),
+}
+
 struct Solution {
     monkeys: HashMap<String, Job>,
 }
@@ -36,10 +44,60 @@ impl Solution {
     fn yell_number(&self, name: &str) -> i64 {
         match &self.monkeys[name] {
             Job::Number(n) => *n,
-            Job::Add(a, b) => self.yell_number(a) + self.yell_number(b),
-            Job::Sub(a, b) => self.yell_number(a) - self.yell_number(b),
-            Job::Mul(a, b) => self.yell_number(a) * self.yell_number(b),
-            Job::Div(a, b) => self.yell_number(a) / self.yell_number(b),
+            Job::Add(lhs, rhs) => self.yell_number(lhs) + self.yell_number(rhs),
+            Job::Sub(lhs, rhs) => self.yell_number(lhs) - self.yell_number(rhs),
+            Job::Mul(lhs, rhs) => self.yell_number(lhs) * self.yell_number(rhs),
+            Job::Div(lhs, rhs) => self.yell_number(lhs) / self.yell_number(rhs),
+        }
+    }
+    fn eval(&self, name: &str) -> Expression {
+        if name == "humn" {
+            return Expression::Human;
+        }
+        match &self.monkeys[name] {
+            Job::Number(n) => Expression::Number(*n),
+            Job::Add(lhs, rhs) => match (self.eval(lhs), self.eval(rhs)) {
+                (Expression::Number(nl), Expression::Number(nr)) => Expression::Number(nl + nr),
+                (lhs, rhs) => Expression::Add(Box::new(lhs), Box::new(rhs)),
+            },
+            Job::Sub(lhs, rhs) => match (self.eval(lhs), self.eval(rhs)) {
+                (Expression::Number(nl), Expression::Number(nr)) => Expression::Number(nl - nr),
+                (lhs, rhs) => Expression::Sub(Box::new(lhs), Box::new(rhs)),
+            },
+            Job::Mul(lhs, rhs) => match (self.eval(lhs), self.eval(rhs)) {
+                (Expression::Number(nl), Expression::Number(nr)) => Expression::Number(nl * nr),
+                (lhs, rhs) => Expression::Mul(Box::new(lhs), Box::new(rhs)),
+            },
+            Job::Div(lhs, rhs) => match (self.eval(lhs), self.eval(rhs)) {
+                (Expression::Number(nl), Expression::Number(nr)) => Expression::Number(nl / nr),
+                (lhs, rhs) => Expression::Div(Box::new(lhs), Box::new(rhs)),
+            },
+        }
+    }
+    fn solve(exp: Expression, target: i64) -> i64 {
+        match exp {
+            Expression::Human => target,
+            Expression::Add(lhs, rhs) => match (*lhs, *rhs) {
+                (Expression::Number(n), exp) => Self::solve(exp, target - n),
+                (exp, Expression::Number(n)) => Self::solve(exp, target - n),
+                _ => unreachable!(),
+            },
+            Expression::Sub(lhs, rhs) => match (*lhs, *rhs) {
+                (Expression::Number(n), exp) => Self::solve(exp, n - target),
+                (exp, Expression::Number(n)) => Self::solve(exp, n + target),
+                _ => unreachable!(),
+            },
+            Expression::Mul(lhs, rhs) => match (*lhs, *rhs) {
+                (Expression::Number(n), exp) => Self::solve(exp, target / n),
+                (exp, Expression::Number(n)) => Self::solve(exp, target / n),
+                _ => unreachable!(),
+            },
+            Expression::Div(lhs, rhs) => match (*lhs, *rhs) {
+                (Expression::Number(n), exp) => Self::solve(exp, n / target),
+                (exp, Expression::Number(n)) => Self::solve(exp, n * target),
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
         }
     }
 }
@@ -64,13 +122,21 @@ impl Solve for Solution {
         self.yell_number("root")
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        let (lhs, rhs) = match &self.monkeys["root"] {
+            Job::Add(lhs, rhs) => (lhs, rhs),
+            _ => unreachable!(),
+        };
+        match (self.eval(lhs), self.eval(rhs)) {
+            (Expression::Number(n), exp) | (exp, Expression::Number(n)) => Self::solve(exp, n),
+            _ => unreachable!(),
+        }
     }
 }
 
 fn main() {
     let solution = Solution::new(std::io::stdin().lock());
     println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
 }
 
 #[cfg(test)]
@@ -101,5 +167,10 @@ hmdt: 32
     #[test]
     fn part1() {
         assert_eq!(152, Solution::new(example_input()).part1());
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(301, Solution::new(example_input()).part2());
     }
 }
