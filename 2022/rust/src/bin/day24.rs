bcdf2d8c108fd5ecd36b98e057052b94594bb974
sugyan <sugi1982@gmail.com>
2022-12-25 00:19:09 +0900

    Add 2022/rust/day24 part2

--- 2022/rust/src/bin/day24.rs
+++ 2022/rust/src/bin/day24.rs
@@ -6,6 +6,56 @@ const NO_BLIZZARD: [bool; 4] = [false, false, false, false];
 
 struct Solution {
     valley: Vec<Vec<[bool; 4]>>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Solution {
+    fn fewest_minutes(
+        &self,
+        valley: &mut Vec<Vec<[bool; 4]>>,
+        start: (usize, usize),
+        goal: (usize, usize),
+    ) -> u32 {
+        let mut hs = HashSet::<(usize, usize)>::new();
+        for minutes in 1.. {
+            *valley = (0..self.rows)
+                .map(|i| {
+                    (0..self.cols)
+                        .map(|j| {
+                            [
+                                valley[(i + 1) % self.rows][j][0],
+                                valley[(i + self.rows - 1) % self.rows][j][1],
+                                valley[i][(j + 1) % self.cols][2],
+                                valley[i][(j + self.cols - 1) % self.cols][3],
+                            ]
+                        })
+                        .collect()
+                })
+                .collect();
+            if hs.contains(&goal) {
+                return minutes;
+            }
+            hs = hs
+                .iter()
+                .flat_map(|(i, j)| {
+                    [(!0, 0), (0, !0), (0, 0), (0, 1), (1, 0)]
+                        .iter()
+                        .filter_map(|&(di, dj)| {
+                            Some((i.wrapping_add(di), j.wrapping_add(dj))).filter(|(i, j)| {
+                                (0..self.rows).contains(i) && (0..self.cols).contains(j)
+                            })
+                        })
+                        .filter(|&(i, j)| valley[i][j] == NO_BLIZZARD)
+                        .collect::<Vec<_>>()
+                })
+                .collect();
+            if valley[start.0][start.1] == NO_BLIZZARD {
+                hs.insert(start);
+            }
+        }
+        unreachable!();
+    }
 }
 
 impl Solve for Solution {
@@ -34,57 +84,33 @@ impl Solve for Solution {
                         .collect()
                 })
                 .collect(),
+            rows: lines.len() - 2,
+            cols: lines[1].len() - 2,
         }
     }
     fn part1(&self) -> Self::Answer1 {
-        let mut valley = self.valley.clone();
-        let (rows, cols) = (valley.len(), valley[0].len());
-        let mut hs = HashSet::<(usize, usize)>::new();
-        for minutes in 1.. {
-            valley = (0..rows)
-                .map(|i| {
-                    (0..cols)
-                        .map(|j| {
-                            [
-                                valley[(i + 1) % rows][j][0],
-                                valley[(i + rows - 1) % rows][j][1],
-                                valley[i][(j + 1) % cols][2],
-                                valley[i][(j + cols - 1) % cols][3],
-                            ]
-                        })
-                        .collect()
-                })
-                .collect();
-            hs = hs
-                .iter()
-                .flat_map(|(i, j)| {
-                    [(!0, 0), (0, !0), (0, 0), (0, 1), (1, 0)]
-                        .iter()
-                        .filter_map(|&(di, dj)| {
-                            Some((i.wrapping_add(di), j.wrapping_add(dj)))
-                                .filter(|(i, j)| (0..rows).contains(i) && (0..cols).contains(j))
-                        })
-                        .filter(|&(i, j)| valley[i][j] == NO_BLIZZARD)
-                        .collect::<Vec<_>>()
-                })
-                .collect();
-            if valley[0][0] == NO_BLIZZARD {
-                hs.insert((0, 0));
-            }
-            if hs.contains(&(rows - 1, cols - 1)) {
-                return minutes + 1;
-            }
-        }
-        unreachable!();
+        self.fewest_minutes(
+            &mut self.valley.clone(),
+            (0, 0),
+            (self.rows - 1, self.cols - 1),
+        )
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        let mut valley = self.valley.clone();
+        [
+            self.fewest_minutes(&mut valley, (0, 0), (self.rows - 1, self.cols - 1)),
+            self.fewest_minutes(&mut valley, (self.rows - 1, self.cols - 1), (0, 0)),
+            self.fewest_minutes(&mut valley, (0, 0), (self.rows - 1, self.cols - 1)),
+        ]
+        .iter()
+        .sum()
     }
 }
 
 fn main() {
     let solution = Solution::new(std::io::stdin().lock());
     println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
 }
 
 #[cfg(test)]
@@ -106,5 +132,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(18, Solution::new(example_input()).part1());
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(54, Solution::new(example_input()).part2());
     }
 }
