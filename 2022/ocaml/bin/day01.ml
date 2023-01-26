6a6876a31d97766cdb3ce0766070a8369daa1dee
sugyan <sugi1982@gmail.com>
2023-01-26 23:35:06 +0900

    Update 2022/ocaml/day01

--- 2022/ocaml/bin/day01.ml
+++ 2022/ocaml/bin/day01.ml
@@ -1,19 +1,19 @@
-open Stdio
 open Base
+open Stdio
 
-let solve channel n =
-  In_channel.input_lines channel
-  |> List.group ~break:(fun x _ -> String.equal x "")
-  |> List.map ~f:(fun l ->
-         List.filter_map
-           ~f:(function "" -> None | x -> Some (Int.of_string x))
-           l
-         |> List.fold ~init:0 ~f:( + ))
-  |> List.sort ~compare:Int.compare
-  |> List.rev
-  |> (fun l -> List.take l n)
-  |> List.fold ~init:0 ~f:( + )
-;;
+let solution input =
+  let sorted_calories =
+    In_channel.fold_lines input ~init:(0, []) ~f:(fun (sum, l) line ->
+        match line with
+        | "" -> (0, sum :: l)
+        | _ -> (sum + Int.of_string line, l))
+    |> (fun (sum, l) -> sum :: l)
+    |> List.sort ~compare:Int.compare
+    |> List.rev |> List.take
+  in
+  fun n -> sorted_calories n |> List.fold ~init:0 ~f:( + )
 
-printf "Part1: %d\n" (solve Stdio.stdin 1)
-(* printf "Part2: %d\n" (solve Stdio.stdin 3) *)
+let _ =
+  let solve = solution Stdio.stdin in
+  printf "Part 1: %d\n" (solve 1);
+  printf "Part 2: %d\n" (solve 3)
