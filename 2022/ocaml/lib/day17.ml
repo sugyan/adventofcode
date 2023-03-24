457c8166a2fb5386fb6188e2a0c07c9afa4b1159
sugyan <sugi1982@gmail.com>
2023-03-25 00:09:56 +0900

    Add 2022/ocaml/day17 part2

--- 2022/ocaml/lib/day17.ml
+++ 2022/ocaml/lib/day17.ml
@@ -24,36 +24,38 @@ module Solution : Solve = struct
            | _ -> failwith "invalid input")
     in
     fun num_rocks ->
-      let to_nth lst =
-        let a, l = List.(to_array lst, length lst) in
-        fun i -> a.(i % l)
-      in
-      let rock_of = to_nth rocks in
-      let jet_of = to_nth jets in
+      let aor, aoj = (Array.of_list rocks, Array.of_list jets) in
+      let len = Array.length aoj in
       let tower = Hash_set.create (module Utils.XY) in
-      let can_place rock (dx, dy) =
-        List.map rock ~f:(fun (x, y) -> (x + dx, y + dy))
-        |> List.for_all ~f:(fun (x, y) ->
-               x >= 0 && x < 7 && y > 0 && not (Hash_set.mem tower (x, y)))
-      in
+      let cache = Hashtbl.create (module Int) in
       let rec loop n j top =
-        if n = num_rocks then top
-        else
-          let r, h = rock_of n in
-          let c = can_place r in
-          let rec fall (x, y) i =
-            ( (match jet_of (j + i) with L -> (x - 1, y) | R -> (x + 1, y))
-            |> fun d -> if c d then d else (x, y) )
-            |> fun (x, y) ->
-            if c (x, y - 1) then fall (x, y - 1) (i + 1) else ((x, y), i + 1)
-          in
-          let (px, py), dj = fall (2, top + 4) 0 in
-          List.map r ~f:(fun (x, y) -> (x + px, y + py))
-          |> List.iter ~f:(Hash_set.add tower);
-          loop (n + 1) (j + dj) (max top (py + h))
+        let key = (j % len * 5) + (n % 5) in
+        Hashtbl.find cache key |> function
+        | Some (n', top') when (num_rocks - n) % (n - n') = 0 ->
+            top + ((top - top') * ((num_rocks - n) / (n - n')))
+        | _ ->
+            Hashtbl.set cache ~key ~data:(n, top);
+            if n = num_rocks then top else simulate n j top
+      and simulate n j top =
+        let r, h = aor.(n % 5) in
+        let c (dx, dy) =
+          List.map r ~f:(fun (x, y) -> (x + dx, y + dy))
+          |> List.for_all ~f:(fun (x, y) ->
+                 x >= 0 && x < 7 && y > 0 && not (Hash_set.mem tower (x, y)))
+        in
+        let rec fall (x, y) i =
+          ( (match aoj.((j + i) % len) with L -> (x - 1, y) | R -> (x + 1, y))
+          |> fun d -> if c d then d else (x, y) )
+          |> fun (x, y) ->
+          if c (x, y - 1) then fall (x, y - 1) (i + 1) else ((x, y), i + 1)
+        in
+        let (x', y'), j' = fall (2, top + 4) 0 in
+        List.map r ~f:(fun (x, y) -> (x + x', y + y'))
+        |> List.iter ~f:(Hash_set.add tower);
+        loop (n + 1) (j + j') (max top (y' + h))
       in
       loop 0 0 0
 
   let part1 tower_height = tower_height 2022 |> answer_of_int
-  let part2 _ = failwith "not implemented"
+  let part2 tower_height = tower_height 1_000_000_000_000 |> answer_of_int
 end
--- 2022/ocaml/test/day17.t/run.t
+++ 2022/ocaml/test/day17.t/run.t
@@ -2,5 +2,5 @@ Part 1
   $ aoc2022 -day 17 -part 1 < example.txt
   Part 1: 3068
 Part 2
-# $ aoc2022 -day 17 -part 2 < example.txt
-# Part 2: 1514285714288
+  $ aoc2022 -day 17 -part 2 < example.txt
+  Part 2: 1514285714288
