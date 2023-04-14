43024885294447211eaa6335cf3993a73c099c86
sugyan <sugi1982@gmail.com>
2023-04-14 23:43:59 +0900

    Improve 2022/ocaml/day19

--- 2022/ocaml/lib/day19.ml
+++ 2022/ocaml/lib/day19.ml
@@ -2,41 +2,48 @@ open Base
 open Solution
 
 module Solution : Solve = struct
-  type blueprint = { costs : int list list; maxs : int list }
+  type blueprint = (int list * int) list
   type t = blueprint list
 
   let max_geodes minutes blueprint =
-    let rec dfs robots resources minutes =
-      List.zip_exn
-        (List.zip_exn blueprint.costs blueprint.maxs)
-        (List.zip_exn robots resources)
-      |> List.mapi ~f:(fun i x -> (i, x))
-      |> List.filter ~f:(fun (i, ((costs, max), (robot, resource))) ->
-             List.zip_exn costs robots
-             |> List.for_all ~f:(fun (cost, robot) -> cost = 0 || robot > 0)
-             && (i = 3 || resource < (max - robot) * minutes))
-      |> List.filter_map ~f:(fun (i, ((costs, _), _)) ->
-             let wait =
-               List.map3_exn robots resources costs
-                 ~f:(fun robot resource cost ->
-                   if robot = 0 || resource >= cost then 0
-                   else ((cost - resource - 1) / robot) + 1)
-               |> List.fold ~init:0 ~f:max
-             in
-             if wait < minutes then Some (i, wait, costs) else None)
-      |> List.map ~f:(fun (i, wait, costs) ->
-             dfs
-               (List.mapi robots ~f:(fun j robot ->
-                    if j = i then robot + 1 else robot))
-               (List.map3_exn robots resources costs
-                  ~f:(fun robot resource cost ->
-                    resource + ((wait + 1) * robot) - cost))
-               (minutes - wait - 1))
-      |> List.fold
-           ~init:(List.nth_exn resources 3 + (List.nth_exn robots 3 * minutes))
-           ~f:max
+    let best = ref 0 in
+    let rec dfs robots_resources minutes =
+      let geodes =
+        List.hd_exn robots_resources |> fun (robot, resource) ->
+        resource + (robot * minutes)
+      in
+      if geodes + ((minutes - 1) * minutes / 2) < !best then ()
+      else (
+        best := max !best geodes;
+        List.zip_exn blueprint robots_resources
+        |> List.mapi ~f:(fun i x -> (i, x))
+        |> List.rev
+        |> List.filter ~f:(fun (i, ((costs, max), (robot, resource))) ->
+               List.zip_exn costs robots_resources
+               |> List.for_all ~f:(fun (cost, (robot, _)) ->
+                      cost = 0 || robot > 0)
+               && (i = 0 || resource < (max - robot) * minutes))
+        |> List.filter_map ~f:(fun (i, ((costs, _), _)) ->
+               let wait =
+                 List.map2_exn robots_resources costs
+                   ~f:(fun (robot, resource) cost ->
+                     if robot = 0 || resource >= cost then 0
+                     else ((cost - resource - 1) / robot) + 1)
+                 |> List.fold ~init:0 ~f:max
+               in
+               if wait < minutes then Some (i, wait, costs) else None)
+        |> List.iter ~f:(fun (i, wait, costs) ->
+               dfs
+                 (List.zip_exn
+                    (List.mapi robots_resources ~f:(fun j (robot, _) ->
+                         if j = i then robot + 1 else robot))
+                    (List.map2_exn robots_resources costs
+                       ~f:(fun (robot, resource) cost ->
+                         resource + ((wait + 1) * robot) - cost)))
+                 (minutes - wait - 1)))
     in
-    dfs [ 1; 0; 0; 0 ] [ 0; 0; 0; 0 ] minutes
+    dfs [ (0, 0); (0, 0); (0, 0); (1, 0) ] minutes;
+    !best
 
   let parse input =
     let parse_line line =
@@ -45,22 +52,13 @@ module Solution : Solve = struct
          ore. Each obsidian robot costs %d ore and %d clay. Each geode robot \
          costs %d ore and %d obsidian."
         (fun _ ore_ore cla_ore obs_ore obs_cla geo_ore geo_obs ->
-          {
-            costs =
-              [
-                [ ore_ore; 0; 0; 0 ];
-                [ cla_ore; 0; 0; 0 ];
-                [ obs_ore; obs_cla; 0; 0 ];
-                [ geo_ore; 0; geo_obs; 0 ];
-              ];
-            maxs =
-              [
-                ore_ore |> max cla_ore |> max obs_ore |> max geo_ore;
-                obs_cla;
-                geo_obs;
-                0;
-              ];
-          })
+          [
+            ([ 0; geo_obs; 0; geo_ore ], 0);
+            ([ 0; 0; obs_cla; obs_ore ], geo_obs);
+            ([ 0; 0; 0; cla_ore ], obs_cla);
+            ( [ 0; 0; 0; ore_ore ],
+              ore_ore |> max cla_ore |> max obs_ore |> max geo_ore );
+          ])
     in
     Stdio.In_channel.input_lines input |> List.map ~f:parse_line
 
