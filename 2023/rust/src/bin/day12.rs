a410a97000917ac7c251096e5ff55dd1d3708089
sugyan <sugi1982@gmail.com>
2023-12-12 22:32:03 +0900

    Add 2023/rust/day12 part2

--- 2023/rust/src/bin/day12.rs
+++ 2023/rust/src/bin/day12.rs
@@ -1,5 +1,5 @@
 use aoc2023::Solve;
-use std::collections::VecDeque;
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read};
 use std::str::FromStr;
 
@@ -29,9 +29,46 @@ struct Solution {
     rows: Vec<Row>,
 }
 
+impl Solution {
+    fn possible_arrangements(s: &[char], target: &[usize]) -> u64 {
+        Self::possible_arrangements_with_memo(s, target, &mut HashMap::new())
+    }
+    fn possible_arrangements_with_memo(
+        s: &[char],
+        target: &[usize],
+        memo: &mut HashMap<(Vec<char>, Vec<usize>), u64>,
+    ) -> u64 {
+        if target.is_empty() {
+            return u64::from(!s.contains(&'#'));
+        }
+        let key = (s.to_vec(), target.to_vec());
+        if let Some(ret) = memo.get(&key) {
+            return *ret;
+        }
+        let mut ret = 0;
+        for i in 0..s.len() {
+            let s = &s[i..];
+            if s.len() < target[0] {
+                break;
+            }
+            if s.len() == target[0] {
+                ret += u64::from(target.len() == 1 && !s.contains(&'.'));
+            } else if !s[0..target[0]].contains(&'.') && s[target[0]] != '#' {
+                ret +=
+                    Self::possible_arrangements_with_memo(&s[target[0] + 1..], &target[1..], memo);
+            }
+            if s[0] == '#' {
+                break;
+            }
+        }
+        memo.insert(key, ret);
+        ret
+    }
+}
+
 impl Solve for Solution {
-    type Answer1 = u32;
-    type Answer2 = u32;
+    type Answer1 = u64;
+    type Answer2 = u64;
 
     fn new(r: impl Read) -> Self {
         Self {
@@ -46,26 +83,23 @@ impl Solve for Solution {
         self.rows
             .iter()
             .map(|row| {
-                let mut count = 0;
-                let mut vd = VecDeque::from([row.springs.clone()]);
-                while let Some(s) = vd.pop_front() {
-                    if let Some((l, r)) = s.split_once('?') {
-                        vd.extend([format!("{l}.{r}"), format!("{l}#{r}")]);
-                    } else if s
-                        .split('.')
-                        .filter_map(|s| if s.is_empty() { None } else { Some(s.len()) })
-                        .collect::<Vec<_>>()
-                        == row.groups
-                    {
-                        count += 1;
-                    }
-                }
-                count
+                Self::possible_arrangements(&row.springs.chars().collect::<Vec<_>>(), &row.groups)
             })
             .sum()
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.rows
+            .iter()
+            .map(|row| {
+                Self::possible_arrangements(
+                    &[row.springs.as_str(); 5]
+                        .join("?")
+                        .chars()
+                        .collect::<Vec<_>>(),
+                    &row.groups.repeat(5),
+                )
+            })
+            .sum()
     }
 }
 
@@ -94,5 +128,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(Solution::new(example_input()).part1(), 21);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(Solution::new(example_input()).part2(), 525_152);
     }
 }
