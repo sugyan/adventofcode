aa16cebc26f9eda825df96a47fd98cfd8fa38c8f
sugyan <sugi1982@gmail.com>
2023-12-18 10:10:45 +0900

    Add 2023/rust/day17 part2

--- 2023/rust/src/bin/day17.rs
+++ 2023/rust/src/bin/day17.rs
@@ -1,20 +1,17 @@
 use aoc2023::Solve;
 use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashMap};
 use std::io::{BufRead, BufReader, Read};
 
-#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 enum Direction {
-    N = 0,
-    E = 1,
-    S = 2,
-    W = 3,
+    N,
+    E,
+    S,
+    W,
 }
 
 impl Direction {
-    fn index(&self) -> usize {
-        *self as usize
-    }
     fn next_directions(&self) -> [Self; 3] {
         match self {
             Self::N => [Self::W, Self::N, Self::E],
@@ -23,18 +20,57 @@ impl Direction {
             Self::W => [Self::S, Self::W, Self::N],
         }
     }
-    fn next_position(&self, (i, j): (usize, usize)) -> (usize, usize) {
-        match self {
-            Self::N => (i.wrapping_sub(1), j),
-            Self::E => (i, j.wrapping_add(1)),
-            Self::S => (i.wrapping_add(1), j),
-            Self::W => (i, j.wrapping_sub(1)),
+    fn next_positions(&self, (mut i, mut j): (usize, usize), steps: usize) -> Vec<(usize, usize)> {
+        let mut ret = Vec::new();
+        for _ in 0..steps {
+            (i, j) = match self {
+                Self::N => (i.wrapping_sub(1), j),
+                Self::E => (i, j.wrapping_add(1)),
+                Self::S => (i.wrapping_add(1), j),
+                Self::W => (i, j.wrapping_sub(1)),
+            };
+            ret.push((i, j));
         }
+        ret
     }
 }
 
 struct Solution {
     map: Vec<Vec<u32>>,
+}
+
+impl Solution {
+    fn minimum_heat_loss(&self, ultra: bool) -> u32 {
+        let (rows, cols) = (self.map.len(), self.map[0].len());
+        let mut mins = vec![vec![HashMap::new(); cols]; rows];
+        let mut bh = BinaryHeap::from([
+            (Reverse(0), (0, 0), (Direction::N, 0)),
+            (Reverse(0), (0, 0), (Direction::W, 0)),
+        ]);
+        while let Some((Reverse(loss), (i, j), (direction, count))) = bh.pop() {
+            if i == rows - 1 && j == cols - 1 {
+                return loss;
+            }
+            for d in direction.next_directions() {
+                if d == direction && count == if ultra { 10 } else { 3 } {
+                    continue;
+                }
+                let steps = if ultra && d != direction { 4 } else { 1 };
+                let positions = d.next_positions((i, j), steps);
+                let &(i, j) = positions.last().expect("should have at least one position");
+                if !(0..rows).contains(&i) || !(0..cols).contains(&j) {
+                    continue;
+                }
+                let l = loss + positions.iter().map(|&(i, j)| self.map[i][j]).sum::<u32>();
+                let c = if d == direction { count + 1 } else { steps };
+                if mins[i][j].get(&(d, c)).map_or(true, |&min| l < min) {
+                    mins[i][j].insert((d, c), l);
+                    bh.push((Reverse(l), (i, j), (d, c)));
+                }
+            }
+        }
+        unreachable!();
+    }
 }
 
 impl Solve for Solution {
@@ -55,33 +91,10 @@ impl Solve for Solution {
         }
     }
     fn part1(&self) -> Self::Answer1 {
-        let (rows, cols) = (self.map.len(), self.map[0].len());
-        let mut mins = vec![vec![[[None; 4]; 4]; cols]; rows];
-        let mut bh = BinaryHeap::from([(Reverse(0), (0, 0), (Direction::E, 0))]);
-        while let Some((Reverse(loss), (i, j), (direction, count))) = bh.pop() {
-            if i == rows - 1 && j == cols - 1 {
-                return loss;
-            }
-            for d in direction.next_directions() {
-                if d == direction && count == 3 {
-                    continue;
-                }
-                let (i, j) = d.next_position((i, j));
-                if !(0..rows).contains(&i) || !(0..cols).contains(&j) {
-                    continue;
-                }
-                let l = loss + self.map[i][j];
-                let c = if d == direction { count + 1 } else { 1 };
-                if mins[i][j][d.index()][c].map_or(true, |min| l < min) {
-                    mins[i][j][d.index()][c] = Some(l);
-                    bh.push((Reverse(l), (i, j), (d, c)));
-                }
-            }
-        }
-        unreachable!();
+        self.minimum_heat_loss(false)
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.minimum_heat_loss(true)
     }
 }
 
@@ -117,5 +130,24 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(Solution::new(example_input()).part1(), 102);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(Solution::new(example_input()).part2(), 94);
+        assert_eq!(
+            Solution::new(
+                r"
+111111111111
+999999999991
+999999999991
+999999999991
+999999999991
+"[1..]
+                    .as_bytes()
+            )
+            .part2(),
+            71
+        );
     }
 }
