5068564c2d0e023d35ab194493830a8a2080fd52
sugyan <sugi1982@gmail.com>
2024-01-30 11:24:20 +0900

    Update 2023/rust/day03

--- 2023/rust/src/bin/day03.rs
+++ 2023/rust/src/bin/day03.rs
@@ -1,12 +1,12 @@
 use aoc2023::Solve;
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read};
 
-type Symbol = (u8, (usize, usize));
+type Point = (usize, usize);
 
 struct Solution {
-    numbers: Vec<(u32, Vec<Symbol>)>,
+    part_numbers: HashMap<(char, Point), Vec<(u32, Point)>>,
 }
 
 impl Solve for Solution {
@@ -17,62 +17,58 @@ impl Solve for Solution {
         let schematic = BufReader::new(r)
             .lines()
             .map_while(Result::ok)
-            .map(|s| s.bytes().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+            .map(|s| s.bytes().collect_vec())
+            .collect_vec();
         let (rows, cols) = (schematic.len(), schematic[0].len());
-        let mut numbers = Vec::new();
+        // collect numbers and their positions
+        let mut numbers = HashMap::new();
         for (i, row) in schematic.iter().enumerate() {
+            let (mut p, mut number) = (None, 0);
             for (j, b) in row.iter().enumerate() {
-                if !b.is_ascii_digit() || (j != 0 && row[j - 1].is_ascii_digit()) {
-                    continue;
+                (p, number) = if b.is_ascii_digit() {
+                    (
+                        Some(*p.get_or_insert((i, j))),
+                        number * 10 + u32::from(b - b'0'),
+                    )
+                } else {
+                    (None, 0)
+                };
+                if let Some(p) = p {
+                    numbers.insert(p, number);
                 }
-                let (mut number, mut len) = (u32::from(b - b'0'), 1);
-                while j + len < cols && schematic[i][j + len].is_ascii_digit() {
-                    number = number * 10 + u32::from(row[j + len] - b'0');
-                    len += 1;
-                }
-                let symbols = ((i.max(1) - 1)..=(i + 1).min(rows - 1))
-                    .cartesian_product((j.max(1) - 1)..=(j + len).min(cols - 1))
-                    .filter_map(|(i, j)| {
-                        let b = schematic[i][j];
-                        if !b.is_ascii_digit() && b != b'.' {
-                            Some((b, (i, j)))
-                        } else {
-                            None
-                        }
-                    })
-                    .collect();
-                numbers.push((number, symbols));
             }
         }
-        Self { numbers }
+        // search symbols adjacent to numbers, and store them in a map
+        let mut part_numbers = HashMap::new();
+        for (&(i, j), n) in &numbers {
+            for p in iproduct!(
+                (i.max(1) - 1)..=(i + 1).min(rows - 1),
+                (j.max(1) - 1)..=(j + n.ilog10() as usize + 1).min(cols - 1)
+            )
+            .filter(|&(i, j)| !schematic[i][j].is_ascii_digit() && schematic[i][j] != b'.')
+            {
+                part_numbers
+                    .entry((schematic[p.0][p.1] as char, p))
+                    .or_insert_with(Vec::new)
+                    .push((*n, (i, j)));
+            }
+        }
+        Self { part_numbers }
     }
     fn part1(&self) -> Self::Answer1 {
-        self.numbers
+        self.part_numbers
             .iter()
-            .filter_map(|(number, symbols)| {
-                if symbols.is_empty() {
-                    None
-                } else {
-                    Some(number)
-                }
-            })
+            .flat_map(|(_, v)| v)
+            .unique()
+            .map(|(n, _)| n)
             .sum()
     }
     fn part2(&self) -> Self::Answer2 {
-        let mut gears = HashMap::new();
-        for (number, symbols) in &self.numbers {
-            for (c, (i, j)) in symbols {
-                if *c == b'*' {
-                    gears.entry((i, j)).or_insert_with(Vec::new).push(*number);
-                }
-            }
-        }
-        gears
-            .values()
-            .filter_map(|v| {
-                if v.len() == 2 {
-                    Some(v[0] * v[1])
+        self.part_numbers
+            .iter()
+            .filter_map(|((c, _), v)| {
+                if *c == '*' && v.len() == 2 {
+                    Some(v[0].0 * v[1].0)
                 } else {
                     None
                 }
