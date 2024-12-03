675952fe5a87b66352c432a1b6c9f0ccb29e6779
sugyan <sugi1982@gmail.com>
2024-12-03 16:23:24 +0900

    Add 2024/rust/day03 part2

--- 2024/rust/src/bin/day03.rs
+++ 2024/rust/src/bin/day03.rs
@@ -13,6 +13,36 @@ struct Solution {
     memory: String,
 }
 
+impl Solution {
+    fn sum_of_multiplications(&self, conditional: bool) -> u32 {
+        let (mut sum, mut enabled) = (0, true);
+        for i in 0..self.memory.len() {
+            if self.memory[i..].starts_with("mul(") {
+                if let Some(value) = self.memory[i + 4..].find(')').and_then(|j| {
+                    let (a, b) = self.memory[i + 4..i + 4 + j]
+                        .split(',')
+                        .map(|s| s.parse::<u32>().ok().filter(|n| *n < 1000))
+                        .collect::<Option<Vec<_>>>()?
+                        .into_iter()
+                        .collect_tuple()?;
+                    Some(a * b)
+                }) {
+                    if !conditional || enabled {
+                        sum += value;
+                    }
+                }
+            }
+            if self.memory[i..].starts_with("do()") {
+                enabled = true;
+            }
+            if self.memory[i..].starts_with("don't()") {
+                enabled = false;
+            }
+        }
+        sum
+    }
+}
+
 impl Solve for Solution {
     type Answer1 = u32;
     type Answer2 = u32;
@@ -27,25 +57,10 @@ impl Solve for Solution {
         Ok(Self { memory })
     }
     fn part1(&self) -> Self::Answer1 {
-        (0..self.memory.len())
-            .filter_map(|i| {
-                if self.memory[i..].starts_with("mul(") {
-                    let j = self.memory[i + 4..].find(')')?;
-                    let (a, b) = self.memory[i + 4..i + 4 + j]
-                        .split(',')
-                        .map(|s| s.parse::<u32>().ok().filter(|n| *n < 1000))
-                        .collect::<Option<Vec<_>>>()?
-                        .into_iter()
-                        .collect_tuple()?;
-                    Some(a * b)
-                } else {
-                    None
-                }
-            })
-            .sum()
+        self.sum_of_multiplications(false)
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.sum_of_multiplications(true)
     }
 }
 
@@ -57,16 +72,29 @@ fn main() -> Result<(), Error> {
 mod tests {
     use super::*;
 
-    fn example_input() -> &'static [u8] {
-        r"
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-"[1..]
-            .as_bytes()
+    #[test]
+    fn part1() -> Result<(), Error> {
+        assert_eq!(
+            Solution::new(
+                r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+                    .as_bytes()
+            )?
+            .part1(),
+            161
+        );
+        Ok(())
     }
 
     #[test]
-    fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part1(), 161);
+    fn part2() -> Result<(), Error> {
+        assert_eq!(
+            Solution::new(
+                r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+                    .as_bytes()
+            )?
+            .part2(),
+            48
+        );
         Ok(())
     }
 }
