a5d4953de5dd1c4c77f84fffd3642bf4a827f825
sugyan <sugi1982@gmail.com>
2024-12-06 15:56:50 +0900

    Add 2024/rust/day06 part2

--- 2024/rust/src/bin/day06.rs
+++ 2024/rust/src/bin/day06.rs
@@ -1,6 +1,6 @@
 use aoc2024::{run, Solve};
 use std::{
-    collections::HashSet,
+    collections::{hash_map::Entry, HashMap, HashSet},
     io::{BufRead, BufReader, Read},
 };
 use thiserror::Error;
@@ -13,13 +13,67 @@ enum Error {
     InvalidInput,
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+        }
+    }
+    fn next_position(&self, (i, j): (usize, usize)) -> (usize, usize) {
+        match self {
+            Self::Up => (i.wrapping_sub(1), j),
+            Self::Right => (i, j.wrapping_add(1)),
+            Self::Down => (i.wrapping_add(1), j),
+            Self::Left => (i, j.wrapping_sub(1)),
+        }
+    }
+}
+
 struct Solution {
     map: Vec<Vec<bool>>,
     guard: (usize, usize),
 }
 
 impl Solution {
-    const DIRECTIONS: [(usize, usize); 4] = [(!0, 0), (0, 1), (1, 0), (0, !0)];
+    fn simulate_patrol(
+        &self,
+        map: &[Vec<bool>],
+    ) -> Option<HashMap<(usize, usize), HashSet<Direction>>> {
+        let (rows, cols) = (map.len(), map[0].len());
+        let ((mut i, mut j), mut dir) = (self.guard, Direction::Up);
+        let mut hm = HashMap::<(usize, usize), HashSet<Direction>>::new();
+        while (0..rows).contains(&i) && (0..cols).contains(&j) {
+            match hm.entry((i, j)) {
+                Entry::Occupied(mut oe) => {
+                    if oe.get().contains(&dir) {
+                        return None;
+                    }
+                    oe.get_mut().insert(dir);
+                }
+                Entry::Vacant(ve) => {
+                    ve.insert(HashSet::new());
+                }
+            }
+            let (ii, jj) = dir.next_position((i, j));
+            if (0..rows).contains(&ii) && (0..cols).contains(&jj) && map[ii][jj] {
+                dir = dir.turn_right();
+            } else {
+                (i, j) = (ii, jj);
+            }
+        }
+        Some(hm)
+    }
 }
 
 impl Solve for Solution {
@@ -46,23 +100,21 @@ impl Solve for Solution {
         })
     }
     fn part1(&self) -> Self::Answer1 {
-        let (rows, cols) = (self.map.len(), self.map[0].len());
-        let ((mut i, mut j), mut dir_iter) = (self.guard, Self::DIRECTIONS.iter().cycle());
-        let (mut di, mut dj) = dir_iter.next().unwrap();
-        let mut hs = HashSet::new();
-        while (0..rows).contains(&i) && (0..cols).contains(&j) {
-            hs.insert((i, j));
-            let (ii, jj) = (i.wrapping_add(di), j.wrapping_add(dj));
-            if (0..rows).contains(&ii) && (0..cols).contains(&jj) && self.map[ii][jj] {
-                (di, dj) = *dir_iter.next().unwrap();
-            } else {
-                (i, j) = (ii, jj);
-            }
-        }
-        hs.len()
+        self.simulate_patrol(&self.map).unwrap().len()
     }
     fn part2(&self) -> Self::Answer2 {
-        todo!()
+        self.simulate_patrol(&self.map)
+            .unwrap()
+            .keys()
+            .filter(|(i, j)| {
+                if (*i, *j) == self.guard {
+                    return false;
+                }
+                let mut map = self.map.clone();
+                map[*i][*j] = true;
+                self.simulate_patrol(&map).is_none()
+            })
+            .count()
     }
 }
 
@@ -93,6 +145,12 @@ mod tests {
     #[test]
     fn part1() -> Result<(), Error> {
         assert_eq!(Solution::new(example_input())?.part1(), 41);
+        Ok(())
+    }
+
+    #[test]
+    fn part2() -> Result<(), Error> {
+        assert_eq!(Solution::new(example_input())?.part2(), 6);
         Ok(())
     }
 }
