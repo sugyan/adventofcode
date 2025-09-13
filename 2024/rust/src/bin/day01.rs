f407d9a743179645bf68b557ff106742a907af8b
sugyan <sugi1982@gmail.com>
2025-09-14 00:11:25 +0900

    Update 2024/rust: Add traits, update day01

--- 2024/rust/src/bin/day01.rs
+++ 2024/rust/src/bin/day01.rs
@@ -1,6 +1,6 @@
-use aoc2024::{Solve, run};
+use aoc2024::{Day, run_day};
 use itertools::Itertools;
-use std::io::{BufRead, BufReader, Read};
+use std::io::BufRead;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -13,20 +13,21 @@ enum Error {
     InvalidInput,
 }
 
-struct Solution {
-    sorted_pairs: (Vec<u32>, Vec<u32>),
+struct Input {
+    l: Vec<u32>,
+    r: Vec<u32>,
 }
 
-impl Solve for Solution {
+struct Solution;
+
+impl Day for Solution {
+    type Input = Input;
     type Answer1 = u32;
     type Answer2 = u32;
     type Error = Error;
 
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let transposed = BufReader::new(r)
+    fn parse<R: BufRead>(r: R) -> Result<Self::Input, Self::Error> {
+        let transposed = r
             .lines()
             .map(|line| {
                 line.map_err(Error::Io)?
@@ -38,25 +39,25 @@ impl Solve for Solution {
             .collect::<Result<Vec<(u32, u32)>, _>>()?
             .into_iter()
             .unzip::<_, _, Vec<_>, Vec<_>>();
-        Ok(Self {
-            sorted_pairs: (
-                transposed.0.into_iter().sorted().collect(),
-                transposed.1.into_iter().sorted().collect(),
-            ),
+        Ok(Input {
+            l: transposed.0.into_iter().sorted().collect(),
+            r: transposed.1.into_iter().sorted().collect(),
         })
     }
-    fn part1(&self) -> Self::Answer1 {
-        self.sorted_pairs
-            .0
+
+    fn part1(input: &Self::Input) -> Self::Answer1 {
+        input
+            .l
             .iter()
-            .zip(&self.sorted_pairs.1)
+            .zip(&input.r)
             .map(|(l, r)| l.abs_diff(*r))
             .sum()
     }
-    fn part2(&self) -> Self::Answer2 {
-        let counts = self.sorted_pairs.1.iter().counts();
-        self.sorted_pairs
-            .0
+
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        let counts = input.r.iter().counts();
+        input
+            .l
             .iter()
             .counts()
             .iter()
@@ -66,15 +67,16 @@ impl Solve for Solution {
 }
 
 fn main() -> Result<(), Error> {
-    run::<Solution>()
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_input() -> &'static [u8] {
-        &r"
+    fn example_input() -> Result<Input, Error> {
+        Solution::parse(
+            &r"
 3   4
 4   3
 2   5
@@ -82,18 +84,19 @@ mod tests {
 3   9
 3   3
 "
-        .as_bytes()[1..]
+            .as_bytes()[1..],
+        )
     }
 
     #[test]
     fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part1(), 11);
+        assert_eq!(Solution::part1(&example_input()?), 11);
         Ok(())
     }
 
     #[test]
     fn part2() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part2(), 31);
+        assert_eq!(Solution::part2(&example_input()?), 31);
         Ok(())
     }
 }
--- 2024/rust/src/lib.rs
+++ 2024/rust/src/lib.rs
@@ -1,8 +1,19 @@
 use std::{
     error::Error,
     fmt::Display,
-    io::{Read, stdin},
+    io::{BufRead, stdin},
 };
+
+pub trait Day {
+    type Input;
+    type Answer1: Display;
+    type Answer2: Display;
+    type Error: Error;
+
+    fn parse<R: BufRead>(r: R) -> Result<Self::Input, Self::Error>;
+    fn part1(input: &Self::Input) -> Self::Answer1;
+    fn part2(input: &Self::Input) -> Self::Answer2;
+}
 
 pub trait Solve: Sized {
     type Answer1: Display;
@@ -11,17 +22,43 @@ pub trait Solve: Sized {
 
     fn new<R>(r: R) -> Result<Self, Self::Error>
     where
-        R: Read;
+        R: BufRead;
     fn part1(&self) -> Self::Answer1;
     fn part2(&self) -> Self::Answer2;
+}
+
+pub fn run_day_with<D, R>(r: R) -> Result<(), D::Error>
+where
+    D: Day,
+    R: BufRead,
+{
+    let data = D::parse(r)?;
+    println!("Part 1: {}", D::part1(&data));
+    println!("Part 2: {}", D::part2(&data));
+    Ok(())
+}
+
+pub fn run_day<D>() -> Result<(), D::Error>
+where
+    D: Day,
+{
+    run_day_with::<D, _>(stdin().lock())
+}
+
+pub fn run_with<S, R>(r: R) -> Result<(), S::Error>
+where
+    S: Solve,
+    R: BufRead,
+{
+    let solution = S::new(r)?;
+    println!("Part 1: {}", solution.part1());
+    println!("Part 2: {}", solution.part2());
+    Ok(())
 }
 
 pub fn run<S>() -> Result<(), S::Error>
 where
     S: Solve,
 {
-    let solution = S::new(stdin().lock())?;
-    println!("Part 1: {}", solution.part1());
-    println!("Part 2: {}", solution.part2());
-    Ok(())
+    run_with::<S, _>(stdin().lock())
 }
