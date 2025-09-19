c26df7be2d52e754ed52e7ee52f043a84758d923
sugyan <sugi1982@gmail.com>
2025-09-19 23:48:49 +0900

    Update 2024/rust/day03

--- 2024/rust/src/bin/day03.rs
+++ 2024/rust/src/bin/day03.rs
@@ -1,76 +1,78 @@
-use aoc2024::{Solve, run};
+use aoc2024::{Day, run_day};
 use itertools::Itertools;
-use std::io::{BufReader, Read};
+use std::str::FromStr;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
+enum Error {}
+
+struct Input(String);
+
+impl FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.lines().join("")))
+    }
 }
 
-struct Solution {
-    memory: String,
-}
+struct Solution;
 
 impl Solution {
-    fn sum_of_multiplications(&self, conditional: bool) -> u32 {
-        let (mut sum, mut enabled) = (0, true);
-        for i in 0..self.memory.len() - 4 {
-            let s = &self.memory[i..];
-            match &s[..4] {
-                "mul(" if !conditional || enabled => {
-                    sum += s
-                        .find(')')
+    fn sum_of_multiplications(input: &Input, conditional: bool) -> u32 {
+        let (mut sum, mut enabled, mut i) = (0, true, 0);
+        let bytes = input.0.as_bytes();
+        while i < bytes.len() {
+            match bytes.get(i..i + 4) {
+                Some(b"mul(") if !conditional || enabled => {
+                    sum += bytes[i..]
+                        .iter()
+                        .position(|&b| b == b')')
                         .and_then(|j| {
-                            s[4..j]
-                                .split(',')
-                                .map(|s| s.parse().ok())
-                                .collect::<Option<Vec<u32>>>()?
+                            Self::parse_numbers(&bytes[i + 4..i + j])?
                                 .into_iter()
                                 .collect_tuple()
                                 .map(|(x, y)| x * y)
                         })
-                        .unwrap_or_default()
+                        .unwrap_or_default();
+                    i += 4;
                 }
-                "do()" => {
+                Some(b"do()") => {
                     enabled = true;
+                    i += 4;
                 }
-                "don'" if s.starts_with("don't()") => {
+                Some(b"don'") if bytes.get(i + 4..i + 7) == Some(b"t()") => {
                     enabled = false;
+                    i += 7;
                 }
-                _ => {}
+                _ => i += 1,
             }
         }
         sum
     }
+    fn parse_numbers(s: &[u8]) -> Option<Vec<u32>> {
+        s.split(|&b| b == b',')
+            .map(|s| str::from_utf8(s).ok()?.parse().ok())
+            .collect()
+    }
 }
 
-impl Solve for Solution {
+impl Day for Solution {
+    type Input = Input;
+    type Error = Error;
     type Answer1 = u32;
     type Answer2 = u32;
-    type Error = Error;
 
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let mut memory = String::new();
-        BufReader::new(r).read_to_string(&mut memory)?;
-        Ok(Self {
-            memory: memory.trim().to_string(),
-        })
+    fn part1(input: &Self::Input) -> Self::Answer1 {
+        Self::sum_of_multiplications(input, false)
     }
-    fn part1(&self) -> Self::Answer1 {
-        self.sum_of_multiplications(false)
-    }
-    fn part2(&self) -> Self::Answer2 {
-        self.sum_of_multiplications(true)
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        Self::sum_of_multiplications(input, true)
     }
 }
 
-fn main() -> Result<(), Error> {
-    run::<Solution>()
+fn main() -> Result<(), aoc2024::Error<Error>> {
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
@@ -80,12 +82,11 @@ mod tests {
     #[test]
     fn part1() -> Result<(), Error> {
         assert_eq!(
-            Solution::new(
+            Solution::part1(
                 &r"
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-                    .as_bytes()[1..]
-            )?
-            .part1(),
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"[1..]
+                    .parse()?
+            ),
             161
         );
         Ok(())
@@ -94,12 +95,11 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     #[test]
     fn part2() -> Result<(), Error> {
         assert_eq!(
-            Solution::new(
+            Solution::part2(
                 &r"
-xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-                    .as_bytes()[1..]
-            )?
-            .part2(),
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"[1..]
+                    .parse()?
+            ),
             48
         );
         Ok(())
