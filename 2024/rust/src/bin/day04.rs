bcdf91789c82062e9a60a99a87ee82491961a632
sugyan <sugi1982@gmail.com>
2025-09-21 22:53:23 +0900

    Update 2024/rust/day04

--- 2024/rust/src/bin/day04.rs
+++ 2024/rust/src/bin/day04.rs
@@ -1,47 +1,22 @@
-use aoc2024::{Solve, run};
-use std::{
-    collections::HashMap,
-    io::{BufRead, BufReader, Read},
-};
+use aoc2024::{Day, run_day};
+use std::{collections::HashMap, str::FromStr};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-}
+enum Error {}
 
-struct Solution {
-    letters: HashMap<(i32, i32), u8>,
-}
+struct Input(HashMap<(i32, i32), u8>);
 
-impl Solution {
-    fn search_word(&self, (i, j): (i32, i32), checks: &[((i32, i32), u8)]) -> bool {
-        checks
-            .iter()
-            .all(|((di, dj), u)| self.letters.get(&(i + di, j + dj)) == Some(u))
-    }
-}
+impl FromStr for Input {
+    type Err = Error;
 
-impl Solve for Solution {
-    type Answer1 = usize;
-    type Answer2 = usize;
-    type Error = Error;
-
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let grid = BufReader::new(r)
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let grid = s
             .lines()
-            .map(|line| {
-                line.map_err(Error::Io)
-                    .map(|line| line.bytes().collect::<Vec<_>>())
-            })
-            .collect::<Result<Vec<_>, _>>()?;
-        Ok(Self {
-            letters: grid
-                .iter()
+            .map(|line| line.bytes().collect())
+            .collect::<Vec<Vec<_>>>();
+        Ok(Self(
+            grid.iter()
                 .enumerate()
                 .flat_map(|(i, row)| {
                     row.iter()
@@ -49,9 +24,27 @@ impl Solve for Solution {
                         .map(move |(j, &c)| ((i as i32, j as i32), c))
                 })
                 .collect(),
-        })
+        ))
     }
-    fn part1(&self) -> Self::Answer1 {
+}
+
+struct Solution;
+
+impl Solution {
+    fn search_word(input: &Input, (i, j): (i32, i32), checks: &[((i32, i32), u8)]) -> bool {
+        checks
+            .iter()
+            .all(|((di, dj), u)| input.0.get(&(i + di, j + dj)) == Some(u))
+    }
+}
+
+impl Day for Solution {
+    type Input = Input;
+    type Error = Error;
+    type Answer1 = usize;
+    type Answer2 = usize;
+
+    fn part1(input: &Self::Input) -> Self::Answer1 {
         let directions = [
             [((1, 0), b'M'), ((2, 0), b'A'), ((3, 0), b'S')],
             [((1, 1), b'M'), ((2, 2), b'A'), ((3, 3), b'S')],
@@ -62,14 +55,15 @@ impl Solve for Solution {
             [((0, -1), b'M'), ((0, -2), b'A'), ((0, -3), b'S')],
             [((1, -1), b'M'), ((2, -2), b'A'), ((3, -3), b'S')],
         ];
-        self.letters
+        input
+            .0
             .iter()
             .filter_map(|(&p, u)| {
                 if *u == b'X' {
                     Some(
                         directions
                             .into_iter()
-                            .filter(|checks| self.search_word(p, checks))
+                            .filter(|checks| Self::search_word(input, p, checks))
                             .count(),
                     )
                 } else {
@@ -78,32 +72,33 @@ impl Solve for Solution {
             })
             .sum()
     }
-    fn part2(&self) -> Self::Answer2 {
+    fn part2(input: &Self::Input) -> Self::Answer2 {
         let ms0 = [((-1, -1), b'M'), ((1, 1), b'S')];
         let sm0 = [((-1, -1), b'S'), ((1, 1), b'M')];
         let ms1 = [((1, -1), b'M'), ((-1, 1), b'S')];
         let sm1 = [((1, -1), b'S'), ((-1, 1), b'M')];
-        self.letters
+        input
+            .0
             .iter()
             .filter(|&(&p, u)| {
                 *u == b'A'
-                    && (self.search_word(p, &ms0) || self.search_word(p, &sm0))
-                    && (self.search_word(p, &ms1) || self.search_word(p, &sm1))
+                    && (Self::search_word(input, p, &ms0) || Self::search_word(input, p, &sm0))
+                    && (Self::search_word(input, p, &ms1) || Self::search_word(input, p, &sm1))
             })
             .count()
     }
 }
 
-fn main() -> Result<(), Error> {
-    run::<Solution>()
+fn main() -> Result<(), aoc2024::Error<Error>> {
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_input() -> &'static [u8] {
-        &r"
+    fn example_input() -> Result<Input, Error> {
+        r"
 MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -114,19 +109,19 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
-"
-        .as_bytes()[1..]
+"[1..]
+            .parse()
     }
 
     #[test]
     fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part1(), 18);
+        assert_eq!(Solution::part1(&example_input()?), 18);
         Ok(())
     }
 
     #[test]
     fn part2() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part2(), 9);
+        assert_eq!(Solution::part2(&example_input()?), 9);
         Ok(())
     }
 }
