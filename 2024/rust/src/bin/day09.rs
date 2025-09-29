04e068d85932879f9a6c467397196328f8c02d1b
sugyan <sugi1982@gmail.com>
2025-09-29 23:31:21 +0900

    Update 2024/rust/day09

--- 2024/rust/src/bin/day09.rs
+++ 2024/rust/src/bin/day09.rs
@@ -1,95 +1,129 @@
-use aoc2024::{Solve, run};
-use std::io::{BufReader, Read};
+use aoc2024::{Day, run_day};
+use std::{array, cmp::Reverse, collections::BinaryHeap, str::FromStr};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
+enum Error {}
+
+struct Input(Vec<usize>);
+
+impl FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(
+            s.trim().bytes().map(|u| usize::from(u - b'0')).collect(),
+        ))
+    }
 }
 
-struct Solution {
-    disk_map: Vec<usize>,
+#[derive(Clone, Debug)]
+struct File {
+    id: usize,
+    position: usize,
+    length: usize,
 }
+
+impl File {
+    fn calculate_checksum(&self, pos: usize, len: usize) -> usize {
+        self.id * (pos + pos + len - 1) * len / 2
+    }
+}
+
+struct Solution;
 
 impl Solution {
-    fn resulting_checksum(&self, move_whole: bool) -> usize {
-        let mut disk_map = self.disk_map.clone();
-        let mut offsets = vec![0; disk_map.len()];
-        for i in 1..disk_map.len() {
-            offsets[i] = offsets[i - 1] + disk_map[i - 1];
+    fn extract_files_and_frees(input: &Input) -> (Vec<File>, [BinaryHeap<Reverse<usize>>; 10]) {
+        let mut files = Vec::with_capacity(input.0.len() / 2);
+        let mut frees = array::from_fn(|_| BinaryHeap::new());
+        let mut pos = 0;
+        for (i, digit) in input.0.iter().enumerate() {
+            if i % 2 == 0 {
+                files.push(File {
+                    id: i / 2,
+                    position: pos,
+                    length: *digit,
+                });
+            } else {
+                frees[*digit].push(Reverse(pos));
+            }
+            pos += *digit;
         }
+        (files, frees)
+    }
+    fn resulting_checksum(input: &Input, move_whole: bool) -> usize {
+        let (files, mut frees) = Self::extract_files_and_frees(input);
         let mut sum = 0;
-        for i in (0..self.disk_map.len()).step_by(2).rev() {
-            while let Some(j) = (1..i)
-                .step_by(2)
-                .find(|j| disk_map[*j] > if move_whole { disk_map[i] - 1 } else { 0 })
-            {
-                for _ in 0..disk_map[i].min(disk_map[j]) {
-                    sum += i / 2 * offsets[j];
-                    offsets[j] += 1;
-                    disk_map[i] -= 1;
-                    disk_map[j] -= 1;
-                }
-                if disk_map[i] == 0 {
+        for file in files.iter().rev() {
+            let mut remain = file.length;
+            let need = if move_whole { file.length } else { 1 };
+            while remain > 0 {
+                if let Some((best_s, best_p)) = frees
+                    .iter()
+                    .enumerate()
+                    .skip(need)
+                    .filter_map(|(i, free)| {
+                        free.peek()
+                            .filter(|Reverse(p)| *p < file.position)
+                            .map(|Reverse(p)| (i, *p))
+                    })
+                    .min_by_key(|(_, p)| *p)
+                {
+                    let take = best_s.min(remain);
+                    sum += file.calculate_checksum(best_p, take);
+                    remain -= take;
+                    if let Some(Reverse(pos)) = frees[best_s].pop() {
+                        frees[best_s - take].push(Reverse(pos + take));
+                    }
+                } else {
+                    sum += file.calculate_checksum(file.position, remain);
                     break;
                 }
-            }
-            for j in 0..disk_map[i] {
-                sum += i / 2 * (offsets[i] + j);
             }
         }
         sum
     }
 }
 
-impl Solve for Solution {
+impl Day for Solution {
+    type Input = Input;
+    type Error = Error;
     type Answer1 = usize;
     type Answer2 = usize;
-    type Error = Error;
 
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let mut buf = String::new();
-        BufReader::new(r).read_to_string(&mut buf)?;
-        Ok(Self {
-            disk_map: buf.trim().bytes().map(|u| usize::from(u - b'0')).collect(),
-        })
+    fn part1(input: &Self::Input) -> Self::Answer1 {
+        Self::resulting_checksum(input, false)
     }
-    fn part1(&self) -> Self::Answer1 {
-        self.resulting_checksum(false)
-    }
-    fn part2(&self) -> Self::Answer2 {
-        self.resulting_checksum(true)
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        Self::resulting_checksum(input, true)
     }
 }
 
-fn main() -> Result<(), Error> {
-    run::<Solution>()
+fn main() -> Result<(), aoc2024::Error<Error>> {
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_input() -> &'static [u8] {
-        &r"
+    fn example_input() -> Result<Input, Error> {
+        r"
 2333133121414131402
 "
-        .as_bytes()[1..]
+        .trim_start()
+        .parse()
     }
 
     #[test]
     fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part1(), 1928);
+        assert_eq!(Solution::part1(&example_input()?), 1928);
         Ok(())
     }
 
     #[test]
     fn part2() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part2(), 2858);
+        assert_eq!(Solution::part2(&example_input()?), 2858);
         Ok(())
     }
 }
