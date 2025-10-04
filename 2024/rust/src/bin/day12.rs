ad128ae3671439a98d6e22dc5263ef6bfd17e571
sugyan <sugi1982@gmail.com>
2025-10-04 23:52:01 +0900

    Update 2024/rust/day12

--- 2024/rust/src/bin/day12.rs
+++ 2024/rust/src/bin/day12.rs
@@ -1,42 +1,64 @@
-use aoc2024::{Solve, run};
+use aoc2024::{Day, run_day};
 use itertools::Itertools;
 use std::{
     collections::{HashMap, HashSet, VecDeque},
-    io::{BufRead, BufReader, Read},
+    str::FromStr,
 };
 use thiserror::Error;
 
 const ADJACENTS: [(usize, usize); 4] = [(!0, 0), (1, 0), (0, !0), (0, 1)];
+const DIRECTIONS_8: [(usize, usize); 8] = [
+    (!0, 0),
+    (!0, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, !0),
+    (0, !0),
+    (!0, !0),
+];
 
 #[derive(Error, Debug)]
-enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
+enum Error {}
+
+struct Input(HashMap<(usize, usize), u8>);
+
+impl FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let map = s
+            .lines()
+            .map(|line| line.bytes().collect_vec())
+            .collect_vec();
+        Ok(Self(
+            map.iter()
+                .enumerate()
+                .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, u)| ((i, j), *u)))
+                .collect(),
+        ))
+    }
 }
 
-struct Solution {
-    garden_plots: HashMap<(usize, usize), u8>,
-}
+struct Solution;
 
 impl Solution {
-    fn total_price(&self, bulk_discount: bool) -> usize {
+    fn total_price<F>(input: &Input, f: F) -> usize
+    where
+        F: Fn(&Input, &[(usize, usize)], &u8) -> usize,
+    {
         let mut sum = 0;
-        let mut seen = HashSet::new();
-        for (p, u) in &self.garden_plots {
+        let mut seen = HashSet::with_capacity(input.0.len());
+        for (p, u) in &input.0 {
             if !seen.contains(p) {
-                let area = self.bfs(p, u, &mut seen);
-                sum += area.len()
-                    * if bulk_discount {
-                        self.count_corners(&area, u)
-                    } else {
-                        self.calculate_perimeter(&area, u)
-                    };
+                let area = Self::bfs(input, p, u, &mut seen);
+                sum += area.len() * f(input, &area, u)
             }
         }
         sum
     }
     fn bfs(
-        &self,
+        input: &Input,
         p: &(usize, usize),
         u: &u8,
         seen: &mut HashSet<(usize, usize)>,
@@ -48,7 +70,7 @@ impl Solution {
             area.push(p);
             for (di, dj) in ADJACENTS {
                 let (i, j) = (p.0.wrapping_add(di), p.1.wrapping_add(dj));
-                if self.garden_plots.get(&(i, j)) == Some(u) && !seen.contains(&(i, j)) {
+                if input.0.get(&(i, j)) == Some(u) && !seen.contains(&(i, j)) {
                     seen.insert((i, j));
                     vd.push_back((i, j));
                 }
@@ -56,103 +78,77 @@ impl Solution {
         }
         area
     }
-    fn calculate_perimeter(&self, area: &[(usize, usize)], u: &u8) -> usize {
+    fn calculate_perimeter(input: &Input, area: &[(usize, usize)], u: &u8) -> usize {
         area.iter()
             .cartesian_product(ADJACENTS)
             .filter(|&((i, j), (di, dj))| {
-                self.garden_plots
-                    .get(&(i.wrapping_add(di), j.wrapping_add(dj)))
-                    != Some(u)
+                input.0.get(&(i.wrapping_add(di), j.wrapping_add(dj))) != Some(u)
             })
             .count()
     }
-    fn count_corners(&self, area: &[(usize, usize)], u: &u8) -> usize {
+    fn count_corners(input: &Input, area: &[(usize, usize)], u: &u8) -> usize {
         area.iter()
             .flat_map(|(i, j)| {
-                [
-                    (!0, 0),
-                    (!0, 1),
-                    (0, 1),
-                    (1, 1),
-                    (1, 0),
-                    (1, !0),
-                    (0, !0),
-                    (!0, !0),
-                ]
-                .iter()
-                .map(|&(di, dj)| {
-                    self.garden_plots
-                        .get(&(i.wrapping_add(di), j.wrapping_add(dj)))
-                        != Some(u)
-                })
-                .circular_tuple_windows()
-                .step_by(2)
+                DIRECTIONS_8
+                    .iter()
+                    .map(|&(di, dj)| {
+                        input.0.get(&(i.wrapping_add(di), j.wrapping_add(dj))) != Some(u)
+                    })
+                    .circular_tuple_windows()
+                    .step_by(2)
             })
             .filter(|t| matches!(t, (true, _, true) | (false, true, false)))
             .count()
     }
 }
 
-impl Solve for Solution {
+impl Day for Solution {
+    type Input = Input;
+    type Error = Error;
     type Answer1 = usize;
     type Answer2 = usize;
-    type Error = Error;
 
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let map = BufReader::new(r)
-            .lines()
-            .map(|line| line.map(|s| s.bytes().collect()))
-            .collect::<Result<Vec<Vec<u8>>, _>>()?;
-        Ok(Self {
-            garden_plots: map
-                .iter()
-                .enumerate()
-                .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, u)| ((i, j), *u)))
-                .collect(),
-        })
+    fn part1(input: &Self::Input) -> Self::Answer1 {
+        Self::total_price(input, Self::calculate_perimeter)
     }
-    fn part1(&self) -> Self::Answer1 {
-        self.total_price(false)
-    }
-    fn part2(&self) -> Self::Answer2 {
-        self.total_price(true)
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        Self::total_price(input, Self::count_corners)
     }
 }
 
-fn main() -> Result<(), Error> {
-    run::<Solution>()
+fn main() -> Result<(), aoc2024::Error<Error>> {
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_input_1() -> &'static [u8] {
-        &r"
+    fn example_input_1() -> Result<Input, Error> {
+        r"
 AAAA
 BBCD
 BBCC
 EEEC
 "
-        .as_bytes()[1..]
+        .trim_start()
+        .parse()
     }
 
-    fn example_input_2() -> &'static [u8] {
-        &r"
+    fn example_input_2() -> Result<Input, Error> {
+        r"
 OOOOO
 OXOXO
 OOOOO
 OXOXO
 OOOOO
 "
-        .as_bytes()[1..]
+        .trim_start()
+        .parse()
     }
 
-    fn example_input_3() -> &'static [u8] {
-        &r"
+    fn example_input_3() -> Result<Input, Error> {
+        r"
 RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
@@ -164,24 +160,25 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE
 "
-        .as_bytes()[1..]
+        .trim_start()
+        .parse()
     }
 
     #[test]
     fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input_1())?.part1(), 140);
-        assert_eq!(Solution::new(example_input_2())?.part1(), 772);
-        assert_eq!(Solution::new(example_input_3())?.part1(), 1930);
+        assert_eq!(Solution::part1(&example_input_1()?), 140);
+        assert_eq!(Solution::part1(&example_input_2()?), 772);
+        assert_eq!(Solution::part1(&example_input_3()?), 1930);
         Ok(())
     }
 
     #[test]
     fn part2() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input_1())?.part2(), 80);
-        assert_eq!(Solution::new(example_input_2())?.part2(), 436);
-        assert_eq!(Solution::new(example_input_3())?.part2(), 1206);
+        assert_eq!(Solution::part2(&example_input_1()?), 80);
+        assert_eq!(Solution::part2(&example_input_2()?), 436);
+        assert_eq!(Solution::part2(&example_input_3()?), 1206);
         assert_eq!(
-            Solution::new(
+            Solution::part2(
                 &r"
 EEEEE
 EXXXX
@@ -189,13 +186,13 @@ EEEEE
 EXXXX
 EEEEE
 "
-                .as_bytes()[1..]
-            )?
-            .part2(),
+                .trim_start()
+                .parse()?
+            ),
             236
         );
         assert_eq!(
-            Solution::new(
+            Solution::part2(
                 &r"
 AAAAAA
 AAABBA
@@ -204,9 +201,9 @@ ABBAAA
 ABBAAA
 AAAAAA
 "
-                .as_bytes()[1..]
-            )?
-            .part2(),
+                .trim_start()
+                .parse()?
+            ),
             368
         );
         Ok(())
