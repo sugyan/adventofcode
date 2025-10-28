ce2daea30c6bf28f17ba1586e47a2fadb24095f0
sugyan <sugi1982@gmail.com>
2025-10-28 23:16:11 +0900

    Update 2024/rust/day20

--- 2024/rust/src/bin/day20.rs
+++ 2024/rust/src/bin/day20.rs
@@ -1,26 +1,50 @@
-use aoc2024::{Solve, run};
+use aoc2024::{Day, run_day};
 use std::{
     collections::{HashMap, HashSet, VecDeque},
-    io::{BufRead, BufReader, Read},
+    str::FromStr,
 };
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
     #[error("invalid input")]
     InvalidInput,
 }
 
-struct Solution {
+struct Input {
     racetrack: HashSet<(isize, isize)>,
     start: (isize, isize),
 }
 
+impl FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines = s.lines().collect::<Vec<_>>();
+        let mut racetrack = HashSet::new();
+        let mut start = None;
+        for (i, row) in (0..).zip(&lines) {
+            for (j, u) in (0..).zip(row.bytes()) {
+                if u == b'S' {
+                    start = Some((i, j));
+                }
+                if u != b'#' {
+                    racetrack.insert((i, j));
+                }
+            }
+        }
+        Ok(Self {
+            racetrack,
+            start: start.ok_or(Error::InvalidInput)?,
+        })
+    }
+}
+
+struct Solution;
+
 impl Solution {
-    fn cheat_counts(&self, seconds: isize, threashold: isize) -> usize {
-        let mins = self.bfs();
+    fn cheat_counts(input: &Input, seconds: isize, threshold: isize) -> usize {
+        let mins = Self::bfs(input);
         let mut sum = 0;
         for (&(i, j), min_src) in &mins {
             sum += (-seconds..=seconds)
@@ -30,21 +54,21 @@ impl Solution {
                 })
                 .filter(|(di, dj)| {
                     mins.get(&(i + di, j + dj))
-                        .map(|min_dst| min_src + di.abs() + dj.abs() + threashold <= *min_dst)
+                        .map(|min_dst| min_src + di.abs() + dj.abs() + threshold <= *min_dst)
                         .unwrap_or_default()
                 })
                 .count();
         }
         sum
     }
-    fn bfs(&self) -> HashMap<(isize, isize), isize> {
-        let mut mins = [(self.start, 0)].into_iter().collect::<HashMap<_, _>>();
-        let mut vd = [(self.start, 0)].into_iter().collect::<VecDeque<_>>();
+    fn bfs(input: &Input) -> HashMap<(isize, isize), isize> {
+        let mut mins = [(input.start, 0)].into_iter().collect::<HashMap<_, _>>();
+        let mut vd = [(input.start, 0)].into_iter().collect::<VecDeque<_>>();
         while let Some((p, d)) = vd.pop_front() {
             mins.insert(p, d);
             for (di, dj) in [(!0, 0), (1, 0), (0, !0), (0, 1)] {
                 let (i, j) = (p.0.wrapping_add(di), p.1.wrapping_add(dj));
-                if self.racetrack.contains(&(i, j)) && !mins.contains_key(&(i, j)) {
+                if input.racetrack.contains(&(i, j)) && !mins.contains_key(&(i, j)) {
                     vd.push_back(((i, j), d + 1));
                 }
             }
@@ -53,55 +77,30 @@ impl Solution {
     }
 }
 
-impl Solve for Solution {
+impl Day for Solution {
+    type Input = Input;
+    type Error = Error;
     type Answer1 = usize;
     type Answer2 = usize;
-    type Error = Error;
 
-    fn new<R>(r: R) -> Result<Self, Error>
-    where
-        R: Read,
-    {
-        let lines = BufReader::new(r).lines().collect::<Result<Vec<_>, _>>()?;
-        let mut racetrack = HashSet::new();
-        for (i, row) in (0..).zip(&lines) {
-            for (j, u) in (0..).zip(row.bytes()) {
-                if u != b'#' {
-                    racetrack.insert((i, j));
-                }
-            }
-        }
-        Ok(Self {
-            racetrack,
-            start: (0..)
-                .zip(lines)
-                .find_map(|(i, row)| {
-                    (0..)
-                        .zip(row.bytes())
-                        .find(|(_, u)| *u == b'S')
-                        .map(|(j, _)| (i, j))
-                })
-                .ok_or(Error::InvalidInput)?,
-        })
+    fn part1(input: &Self::Input) -> Self::Answer1 {
+        Self::cheat_counts(input, 2, 100)
     }
-    fn part1(&self) -> Self::Answer1 {
-        self.cheat_counts(2, 100)
-    }
-    fn part2(&self) -> Self::Answer2 {
-        self.cheat_counts(20, 100)
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        Self::cheat_counts(input, 20, 100)
     }
 }
 
-fn main() -> Result<(), Error> {
-    run::<Solution>()
+fn main() -> Result<(), aoc2024::Error<Error>> {
+    run_day::<Solution>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn example_input() -> &'static [u8] {
-        &r"
+    fn example_input() -> Result<Input, Error> {
+        r"
 ###############
 #...#...#.....#
 #.#.#.#.#.###.#
@@ -118,50 +117,51 @@ mod tests {
 #...#...#...###
 ###############
 "
-        .as_bytes()[1..]
+        .trim_start()
+        .parse()
     }
 
     #[test]
     fn cheat_counts() -> Result<(), Error> {
         // 2
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 64), 1);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 40), 2);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 38), 3);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 36), 4);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 20), 5);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 12), 8);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 10), 10);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 8), 14);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 6), 16);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 4), 30);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(2, 2), 44);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 64), 1);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 40), 2);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 38), 3);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 36), 4);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 20), 5);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 12), 8);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 10), 10);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 8), 14);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 6), 16);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 4), 30);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 2, 2), 44);
         // 20
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 76), 3);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 74), 7);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 72), 29);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 70), 41);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 68), 55);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 66), 67);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 64), 86);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 62), 106);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 60), 129);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 58), 154);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 56), 193);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 54), 222);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 52), 253);
-        assert_eq!(Solution::new(example_input())?.cheat_counts(20, 50), 285);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 76), 3);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 74), 7);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 72), 29);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 70), 41);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 68), 55);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 66), 67);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 64), 86);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 62), 106);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 60), 129);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 58), 154);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 56), 193);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 54), 222);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 52), 253);
+        assert_eq!(Solution::cheat_counts(&example_input()?, 20, 50), 285);
         Ok(())
     }
 
     #[test]
     fn part1() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part1(), 0);
+        assert_eq!(Solution::part1(&example_input()?), 0);
         Ok(())
     }
 
     #[test]
     fn part2() -> Result<(), Error> {
-        assert_eq!(Solution::new(example_input())?.part2(), 0);
+        assert_eq!(Solution::part2(&example_input()?), 0);
         Ok(())
     }
 }
