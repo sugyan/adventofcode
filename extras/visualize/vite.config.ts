7ee3b377fbaa7f1820dad774444a53f64d55cc34
sugyan <sugi1982@gmail.com>
2025-11-14 15:44:34 +0900

    Add workflows

--- extras/visualize/vite.config.ts
+++ extras/visualize/vite.config.ts
@@ -10,7 +10,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         "2024-day14": resolve(__dirname, "2024/day14/index.html"),
+        "2024-day18": resolve(__dirname, "2024/day18/index.html"),
       },
     },
   },
+  base: process.env.VITE_BASENAME,
 });
