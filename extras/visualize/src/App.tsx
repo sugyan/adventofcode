f41924a6de94bd79c720d0e5e34bdb43721a0953
sugyan <sugi1982@gmail.com>
2025-11-14 16:13:24 +0900

    Fix App.tsx

--- extras/visualize/src/App.tsx
+++ extras/visualize/src/App.tsx
@@ -3,10 +3,14 @@ function App() {
     <>
       <ul>
         <li>
-          <a href="/2024/day14/">2024 day14</a>
+          <a href={`${import.meta.env.VITE_BASENAME || ""}/2024/day14/`}>
+            2024 day14
+          </a>
         </li>
         <li>
-          <a href="/2024/day18/">2024 day18</a>
+          <a href={`${import.meta.env.VITE_BASENAME || ""}/2024/day18/`}>
+            2024 day18
+          </a>
         </li>
       </ul>
     </>
