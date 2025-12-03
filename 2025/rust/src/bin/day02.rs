5ec7bd992f559e110511486ed964e8e54c4d0c3f
sugyan <sugi1982@gmail.com>
2025-12-03 23:17:01 +0900

    Add 2025/rust day02 part2

--- 2025/rust/src/bin/day02.rs
+++ 2025/rust/src/bin/day02.rs
@@ -42,31 +42,53 @@ impl FromStr for Input {
 
 struct Solution;
 
+impl Solution {
+    fn is_digits_repeated_twice(d: &u64) -> bool {
+        let n = 10_u64.pow(d.ilog10().div_ceil(2));
+        d / n == d % n
+    }
+    fn is_digits_repeated_at_least_twice(d: &u64) -> bool {
+        'outer: for i in 1..=d.ilog10().div_ceil(2) {
+            let n = 10_u64.pow(i);
+            let s = d % n;
+            if s == 0 || s.ilog10() + 1 != i {
+                continue;
+            }
+            let mut m = *d;
+            while m > 0 {
+                if m % n != s {
+                    continue 'outer;
+                }
+                m /= n;
+            }
+            return true;
+        }
+        false
+    }
+}
+
 impl Day for Solution {
     type Input = Input;
     type Error = Error;
     type Answer1 = u64;
-    type Answer2 = u32;
+    type Answer2 = u64;
 
     fn part1(input: &Self::Input) -> Self::Answer1 {
-        let mut sum = 0;
-        for r in input.0.iter() {
-            for i in r.0..=r.1 {
-                let d = i.ilog10();
-                if d % 2 == 0 {
-                    continue;
-                }
-                let n = 10_u64.pow(d.div_ceil(2));
-                if i / n == i % n {
-                    sum += i;
-                }
-            }
-        }
-        sum
+        input
+            .0
+            .iter()
+            .flat_map(|r| r.0..=r.1)
+            .filter(Self::is_digits_repeated_twice)
+            .sum()
     }
 
-    fn part2(_: &Self::Input) -> Self::Answer2 {
-        todo!()
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        input
+            .0
+            .iter()
+            .flat_map(|r| r.0..=r.1)
+            .filter(Self::is_digits_repeated_at_least_twice)
+            .sum()
     }
 }
 
@@ -91,6 +113,12 @@ mod tests {
     #[test]
     fn part1() -> Result<(), Error> {
         assert_eq!(Solution::part1(&example_input()?), 1_227_775_554);
+        Ok(())
+    }
+
+    #[test]
+    fn part2() -> Result<(), Error> {
+        assert_eq!(Solution::part2(&example_input()?), 4_174_379_265);
         Ok(())
     }
 }
