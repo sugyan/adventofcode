3e1dd447715a51f7f31415f42fb297aa33412da7
sugyan <sugi1982@gmail.com>
2025-12-04 22:52:28 +0900

    Add 2025/rust day03 part2

--- 2025/rust/src/bin/day03.rs
+++ 2025/rust/src/bin/day03.rs
@@ -1,50 +1,67 @@
 use aoc2025::{Day, run};
+use itertools::Itertools;
 use std::str::FromStr;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-enum Error {}
+enum Error {
+    #[error("invalid digit")]
+    InvalidDigit,
+}
 
-struct Input(Vec<String>);
+struct Input(Vec<Vec<u64>>);
 
 impl FromStr for Input {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(s.lines().map(String::from).collect()))
+        Ok(Self(
+            s.lines()
+                .map(|line| {
+                    line.chars()
+                        .map(|c| c.to_digit(10).map(u64::from).ok_or(Error::InvalidDigit))
+                        .try_collect()
+                })
+                .try_collect()?,
+        ))
     }
 }
 
 struct Solution;
 
+impl Solution {
+    fn max_n_digit_number(digits: &[u64], n: usize) -> u64 {
+        let mut dp = vec![vec![0_u64; digits.len() + 1]; n + 1];
+        for i in 1..=n {
+            let n = 10_u64.pow(i as u32 - 1);
+            for j in (0..=digits.len() - i).rev() {
+                dp[i][j] = dp[i][j + 1].max(digits[j] * n + dp[i - 1][j + 1]);
+            }
+        }
+        dp[n][0]
+    }
+}
+
 impl Day for Solution {
     type Input = Input;
     type Error = Error;
-    type Answer1 = u32;
-    type Answer2 = u32;
+    type Answer1 = u64;
+    type Answer2 = u64;
 
     fn part1(input: &Self::Input) -> Self::Answer1 {
         input
             .0
             .iter()
-            .map(|line| {
-                let digits = line
-                    .chars()
-                    .map(|c| c.to_digit(10).unwrap())
-                    .collect::<Vec<_>>();
-                let mut max = 0;
-                for i in 0..digits.len() - 1 {
-                    for j in i + 1..digits.len() {
-                        max = max.max(digits[i] * 10 + digits[j]);
-                    }
-                }
-                max
-            })
+            .map(|v| Solution::max_n_digit_number(v, 2))
             .sum()
     }
 
-    fn part2(_: &Self::Input) -> Self::Answer2 {
-        todo!()
+    fn part2(input: &Self::Input) -> Self::Answer2 {
+        input
+            .0
+            .iter()
+            .map(|v| Solution::max_n_digit_number(v, 12))
+            .sum()
     }
 }
 
@@ -70,6 +87,12 @@ mod tests {
     #[test]
     fn part1() -> Result<(), Error> {
         assert_eq!(Solution::part1(&example_input()?), 357);
+        Ok(())
+    }
+
+    #[test]
+    fn part2() -> Result<(), Error> {
+        assert_eq!(Solution::part2(&example_input()?), 3_121_910_778_619);
         Ok(())
     }
 }
