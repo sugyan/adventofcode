ad9e860ea6bea09a92eae844296f331580a2c681
sugyan <sugi1982@gmail.com>
2025-12-10 13:04:31 +0900

    Update

--- 2025/ocaml/bin/day03.ml
+++ 2025/ocaml/bin/day03.ml
@@ -15,14 +15,13 @@ module Solution : Aoc.Day = struct
     let rec aux n arr =
       if n = batteries then arr.(0)
       else
-        let dp = Array.create ~len:(Array.length arr) 0 in
+        let dp = Array.copy arr in
         for i = Array.length dp - 2 downto 0 do
-          dp.(i) <-
-            (digits.(i) * Int.pow 10 n) + arr.(i + 1) |> Int.max dp.(i + 1)
+          dp.(i) <- (digits.(i) * Int.pow 10 n) + arr.(i + 1) |> max dp.(i + 1)
         done;
         aux (n + 1) (Array.subo dp ~len:(Array.length dp - 1))
     in
-    Array.create ~len:(Array.length digits + 1) 0 |> aux 0
+    Array.(create ~len:(length digits + 1)) 0 |> aux 0
 
   let part1 input =
     List.sum (module Int) input ~f:(max_n_digit_number 2) |> Answer.of_int
